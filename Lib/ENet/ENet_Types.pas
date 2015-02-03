8048fa4ffe284d3331ab5c0782319342b1689ceb
binarymaster <x86corez@gmail.com>
2015-02-03 22:42:14 +0300

    Update ENet headers
    ENet headers translated by Dmitry D. Chernov aka Black Doomer

--- Lib/ENet/ENet_Types.pas
+++ Lib/ENet/ENet_Types.pas
@@ -2,9 +2,32 @@ unit ENet_Types;
 
 {
   ENet - Reliable UDP networking library
-  Delphi 7 DLL header by Chernov D. Dmitry aka Black Doomer
+
+  Delphi 7 DLL header: ENet_Types.pas
+  Copyright (c) 2014 Dmitry D. Chernov aka Black Doomer
+
   Original file: types.h
-  16.08.2014
+  Copyright (c) 2002-2014 Lee Salzman
+
+  Version 1 for 1.3.12: 16.08.2014
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
 }
 
 interface
