f502e4302c56a55d68fb07c90b3220c1c8022fea
binarymaster <x86corez@gmail.com>
2016-01-01 22:52:40 +0300

    Player: Store model and color for bots
    Fix of
    https://github.com/pss88/DF/commit/26644c5e323cf507ccb0e2550c0aabbf9c93bdf4

--- DF Sources/g_player.pas
+++ DF Sources/g_player.pas
@@ -4744,6 +4744,7 @@ procedure TPlayer.SaveState(var Mem: TBinMemoryWriter);
 var
   i: Integer;
   sig: DWORD;
+  str: String;
   b: Byte;
 begin
   if FIamBot then
@@ -4858,6 +4859,16 @@ begin
 // Время до повторного респауна, смены оружия, исользования:
   for i := T_RESPAWN to T_USE do
     Mem.WriteDWORD(FTime[i]);
+// Название модели:
+  str := FModel.Name;
+  Mem.WriteString(str);
+// Цвет модели:
+  b := FModel.Color.R;
+  Mem.WriteByte(b);
+  b := FModel.Color.G;
+  Mem.WriteByte(b);
+  b := FModel.Color.B;
+  Mem.WriteByte(b);
 end;
 
 procedure TPlayer.LoadState(var Mem: TBinMemoryReader);
@@ -4866,6 +4877,7 @@ var
   sig: DWORD;
   str: String;
   b: Byte;
+  col: TRGB;
 begin
   if Mem = nil then
     Exit;
@@ -4970,6 +4982,18 @@ begin
 // Время до повторного респауна, смены оружия, исользования:
   for i := T_RESPAWN to T_USE do
     Mem.ReadDWORD(FTime[i]);
+// Название модели:
+  Mem.ReadString(str);
+// Цвет модели:
+  Mem.ReadByte(col.R);
+  Mem.ReadByte(col.G);
+  Mem.ReadByte(col.B);
+// Обновляем модель игрока (только для ботов):
+  if (FPlayerNum <> 1) and (FPlayerNum <> 2) then
+  begin
+    SetModel(str);
+    FModel.Color := col;
+  end;
 end;
 
 procedure TPlayer.AllRulez(Health: Boolean);
