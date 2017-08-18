fc692e6779ebf28c181f1933333cdaf921c1faba
fgsfds <pvt.fgsfds@gmail.com>
2017-08-18 16:44:15 +0300

    added README

--- build_headless.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd "./src/game"
-fpc -dUSE_FMOD -dHEADLESS -MDELPHI -O2 -Fi../lib/vampimg -Fi../lib/vampimg/JpegLib -Fi../lib/vampimg/ZLib -Fu../lib/vampimg -Fu../lib/vampimg/JpegLib -Fu../lib/vampimg/ZLib -FE../../bin -FU../../tmp -oDoom2DF_H.exe Doom2DF.dpr
-cd "../.."
