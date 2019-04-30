043af909368ca8f82e418f0e5e440478b89eb3d4
DeaDDooMER <deaddoomer@deadsoftware.ru>
2019-04-30 19:54:02 +0300

    fixed chat input AGAIN

--- src/game/g_console.pas
+++ src/game/g_console.pas
@@ -1727,7 +1727,7 @@ end;
 procedure g_Console_ProcessBind (key: Integer; down: Boolean);
   var i: Integer;
 begin
-  if (not g_GUIGrabInput) and (key >= 0) and (key < e_MaxInputKeys) and ((gInputBinds[key].down <> nil) or (gInputBinds[key].up <> nil)) then
+  if (not g_GUIGrabInput) and (not gChatShow) and (key >= 0) and (key < e_MaxInputKeys) and ((gInputBinds[key].down <> nil) or (gInputBinds[key].up <> nil)) then
   begin
     if down then
       for i := 0 to High(gInputBinds[key].down) do
