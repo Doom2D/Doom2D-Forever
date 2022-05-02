8065721d62f9484545707edb2e049b39a0542641
fgsfds <pvt.fgsfds@gmail.com>
2022-05-02 15:01:00 +0300

    graphics: fix screenshots when r_resolution_scale != 1

--- src/game/g_game.pas
+++ src/game/g_game.pas
@@ -7775,7 +7775,7 @@ begin
     name := e_CatPath(dir, Filename + '.png');
     s := createDiskFile(name);
     try
-      e_MakeScreenshot(s, gScreenWidth, gScreenHeight);
+      e_MakeScreenshot(s, gWinSizeX, gWinSizeY);
       s.Free;
       g_Console_Add(Format(_lc[I_CONSOLE_SCREENSHOT], [name]))
     except
