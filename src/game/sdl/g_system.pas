Doom2D/Doom2D-Forever

commit 0e101bd452c40da601236aaa2dd4106be47ddce1

implement SDL1.2 system driver

src/game/sdl/g_system.pas
@@ -1,0 +1,301 @@
+(* Copyright (C)  Doom 2D: Forever Developers
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License ONLY.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *)
+{$INCLUDE ../shared/a_modes.inc}
+unit g_system;
+
+interface
+
+  (* To fix:
+   *   - Text input
+   *   - Joystick support
+   *   - Window resizing using SDL_VIDEORESIZE
+   *)
+
+  uses Utils;
+
+  (* --- Utils --- *)
+  function sys_GetTicks (): Int64;
+  procedure sys_Delay (ms: Integer);
+
+  (* --- Graphics --- *)
+  function sys_GetDispalyModes (bpp: Integer): SSArray;
+  function sys_SetDisplayMode (w, h, bpp: Integer; fullscreen: Boolean): Boolean;
+  procedure sys_EnableVSync (yes: Boolean);
+  procedure sys_Repaint;
+
+  (* --- Input --- *)
+  function sys_HandleInput (): Boolean;
+  procedure sys_RequestQuit;
+
+  (* --- Init --- *)
+  procedure sys_Init;
+  procedure sys_Final;
+
+implementation
+
+  uses
+    SysUtils, SDL, GL,
+    e_log, e_graphics, e_input,
+    g_options, g_window, g_console, g_game, g_menu;
+
+  var
+    screen: PSDL_Surface;
+
+  (* --------- Utils --------- *)
+
+  function sys_GetTicks (): Int64;
+  begin
+    Result := SDL_GetTicks()
+  end;
+
+  procedure sys_Delay (ms: Integer);
+  begin
+    SDL_Delay(ms)
+  end;
+
+  (* --------- Graphics --------- *)
+
+  procedure UpdateSize (w, h: Integer);
+  begin
+    gWinSizeX := w;
+    gWinSizeY := h;
+    gWinRealPosX := 0;
+    gWinRealPosY := 0;
+    gScreenWidth := w;
+    gScreenHeight := h;
+    {$IFDEF ENABLE_HOLMES}
+      fuiScrWdt := w;
+      fuiScrHgt := h;
+    {$ENDIF}
+    e_ResizeWindow(w, h);
+    e_InitGL;
+    g_Game_SetupScreenSize;
+    g_Menu_Reset;
+    g_Game_ClearLoading;
+  end;
+
+  function InitWindow (w, h, bpp: Integer; fullScreen: Boolean): Boolean;
+    var flags: Uint32;
+  begin
+    e_LogWritefln('InitWindow %s %s %s %s', [w, h, bpp, fullscreen]);
+    Result := False;
+    SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 16);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8); // lights; it is enough to have 1-bit stencil buffer for lighting, but...
+    flags := SDL_OPENGL or SDL_VIDEORESIZE;
+    if fullScreen then
+      flags := flags or SDL_FULLSCREEN;
+    if (screen = nil) or (SDL_VideoModeOk(w, h, bpp, flags) <> 0) then
+    begin
+      SDL_FreeSurface(screen);
+      screen := SDL_SetVideoMode(w, h, bpp, flags);
+      if screen <> nil then
+      begin
+        SDL_WM_SetCaption('Doom 2D: Forever (SDL 1.2)', nil);
+        UpdateSize(w, h);
+        Result := True
+      end
+    end
+    else
+    begin
+      e_LogWritefln('SDL: video mode not supported', [])
+    end
+  end;
+
+  procedure sys_Repaint;
+  begin
+    SDL_GL_SwapBuffers
+  end;
+
+  procedure sys_EnableVSync (yes: Boolean);
+  begin
+    (* ??? *)
+  end;
+
+  function sys_GetDispalyModes (bpp: Integer): SSArray;
+    var m: PPSDL_Rect; f: TSDL_PixelFormat; i, count: Integer;
+  begin
+    SetLength(result, 0);
+    FillChar(f, sizeof(f), 0);
+    f.palette := nil;
+    f.BitsPerPixel := bpp;
+    f.BytesPerPixel := (bpp + 7) div 8;
+    m := SDL_ListModes(@f, SDL_OPENGL or SDL_FULLSCREEN);
+    if (m <> NIL) and (IntPtr(m) <> -1) then
+    begin
+      count := 0;
+      while m[count] <> nil do inc(count);
+      SetLength(result, count);
+      for i := 0 to count - 1 do
+        result[i] := IntToStr(m[i].w) + 'x' + IntToStr(m[i].h);
+    end
+  end;
+
+  function sys_SetDisplayMode (w, h, bpp: Integer; fullscreen: Boolean): Boolean;
+  begin
+    result := InitWindow(w, h, bpp, fullscreen)
+  end;
+
+  (* --------- Input --------- *)
+
+  function Key2Stub (key: Integer): Integer;
+    var x: Integer;
+  begin
+    case key of
+      SDLK_ESCAPE: x := IK_ESCAPE;
+      SDLK_RETURN: x := IK_RETURN;
+      SDLK_KP_ENTER: x := IK_KPRETURN;
+      SDLK_KP0: x := IK_KPINSERT;
+      SDLK_UP: x := IK_UP;
+      SDLK_KP8: x := IK_KPUP;
+      SDLK_DOWN: x := IK_DOWN;
+      SDLK_KP2: x := IK_KPDOWN;
+      SDLK_LEFT: x := IK_LEFT;
+      SDLK_KP4: x := IK_KPLEFT;
+      SDLK_RIGHT: x := IK_RIGHT;
+      SDLK_KP6: x := IK_KPRIGHT;
+      SDLK_DELETE: x := IK_DELETE;
+      SDLK_HOME: x := IK_HOME;
+      SDLK_KP7: x := IK_KPHOME;
+      SDLK_INSERT: x := IK_INSERT;
+      SDLK_SPACE: x := IK_SPACE;
+      SDLK_LSHIFT: x := IK_SHIFT;
+      SDLK_LALT: x := IK_ALT;
+      SDLK_TAB: x := IK_TAB;
+      SDLK_PAGEUP: x := IK_PAGEUP;
+      SDLK_KP9: x := IK_KPPAGEUP;
+      SDLK_PAGEDOWN: x := IK_PAGEDN;
+      SDLK_KP3: x := IK_KPPAGEDN;
+      SDLK_KP5: x := IK_KP5;
+      SDLK_NUMLOCK: x := IK_NUMLOCK;
+      SDLK_KP_DIVIDE: x := IK_KPDIVIDE;
+      SDLK_KP_MULTIPLY: x := IK_KPMULTIPLE;
+      SDLK_KP_MINUS: x := IK_KPMINUS;
+      SDLK_KP_PLUS: x := IK_KPPLUS;
+      SDLK_KP_PERIOD: x := IK_KPDOT;
+      SDLK_CAPSLOCK: x := IK_CAPSLOCK;
+      SDLK_RSHIFT: x := IK_RSHIFT;
+      SDLK_LCTRL: x := IK_CTRL;
+      SDLK_RCTRL: x := IK_RCTRL;
+      SDLK_RALT: x := IK_RALT;
+      SDLK_LSUPER: x := IK_WIN;
+      SDLK_RSUPER: x := IK_RWIN;
+      SDLK_MENU: x := IK_MENU;
+      SDLK_PRINT: x := IK_PRINTSCR;
+      SDLK_SCROLLOCK: x := IK_SCROLLLOCK;
+      SDLK_LEFTBRACKET: x := IK_LBRACKET;
+      SDLK_RIGHTBRACKET: x := IK_RBRACKET;
+      SDLK_SEMICOLON: x := IK_SEMICOLON;
+      SDLK_QUOTE: x := IK_QUOTE;
+      SDLK_BACKSLASH: x := IK_BACKSLASH;
+      SDLK_SLASH: x := IK_SLASH;
+      SDLK_COMMA: x := IK_COMMA;
+      SDLK_PERIOD: x := IK_DOT;
+      SDLK_EQUALS: x := IK_EQUALS;
+      SDLK_0: x := IK_0;
+      SDLK_1: x := IK_1;
+      SDLK_2: x := IK_2;
+      SDLK_3: x := IK_3;
+      SDLK_4: x := IK_4;
+      SDLK_5: x := IK_5;
+      SDLK_6: x := IK_6;
+      SDLK_7: x := IK_7;
+      SDLK_8: x := IK_8;
+      SDLK_9: x := IK_9;
+      SDLK_F1: x := IK_F1;
+      SDLK_F2: x := IK_F2;
+      SDLK_F3: x := IK_F3;
+      SDLK_F4: x := IK_F4;
+      SDLK_F5: x := IK_F5;
+      SDLK_F6: x := IK_F6;
+      SDLK_F7: x := IK_F7;
+      SDLK_F8: x := IK_F8;
+      SDLK_F9: x := IK_F9;
+      SDLK_F10: x := IK_F10;
+      SDLK_F11: x := IK_F11;
+      SDLK_F12: x := IK_F12;
+      SDLK_END: x := IK_END;
+      SDLK_KP1: x := IK_KPEND;
+      SDLK_BACKSPACE: x := IK_BACKSPACE;
+      SDLK_BACKQUOTE: x := IK_BACKQUOTE;
+      SDLK_PAUSE: x := IK_PAUSE;
+      SDLK_A..SDLK_Z: x := IK_A + (key - SDLK_A);
+      SDLK_MINUS: x := IK_MINUS;
+    else
+      x := IK_INVALID
+    end;
+    result := x
+  end;
+
+  procedure HandleKeyboard (var ev: TSDL_KeyboardEvent);
+    var down: Boolean; key, stb: Integer;
+  begin
+    down := (ev.type_ = SDL_KEYDOWN);
+    key := ev.keysym.sym;
+    stb := Key2Stub(key);
+    if g_dbg_input then
+      e_LogWritefln('Input Debug: keysym, press=%s, scancode=%s SDL.ev.key.state=%s', [down, key, ev.state]);
+    e_KeyUpDown(stb, down);
+    g_Console_ProcessBind(stb, down);
+    (* !!! need text input -- console, cheaats, fields *)
+  end;
+
+  function sys_HandleInput (): Boolean;
+    var ev: TSDL_Event;
+  begin
+    result := false;
+    while SDL_PollEvent(@ev) <> 0 do
+    begin
+      case ev.type_ of
+        SDL_QUITEV: result := true;
+        SDL_VIDEORESIZE: InitWindow(ev.resize.w, ev.resize.h, gBPP, gFullscreen);
+        SDL_KEYUP, SDL_KEYDOWN: HandleKeyboard(ev.key);
+      end
+    end
+  end;
+
+  procedure sys_RequestQuit;
+    var ev: TSDL_Event;
+  begin
+    ev.quit.type_ := SDL_QUITEV;
+    SDL_PushEvent(@ev)
+  end;
+
+  (* --------- Init --------- *)
+
+  procedure sys_Init;
+    var flags: Uint32; ok: Boolean;
+  begin
+    flags := SDL_INIT_VIDEO or SDL_INIT_AUDIO or
+             SDL_INIT_TIMER or SDL_INIT_JOYSTICK
+             (*or SDL_INIT_NOPARACHUTE*);
+    if SDL_Init(flags) <> 0 then
+      raise Exception.Create('SDL: Init failed: ' + SDL_GetError());
+    ok := InitWindow(gScreenWidth, gScreenHeight, gBPP, gFullScreen);
+    if not ok then
+      raise Exception.Create('SDL: failed to set videomode: ' + SDL_GetError)
+  end;
+
+  procedure sys_Final;
+  begin
+    e_WriteLog('Releasing SDL', TMsgType.Notify);
+    SDL_FreeSurface(screen);
+    SDL_Quit
+  end;
+
+end.

src/game/stub/g_system.pas
@@ -1,0 +1,94 @@
+(* Copyright (C)  Doom 2D: Forever Developers
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License ONLY.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *)
+{$INCLUDE ../shared/a_modes.inc}
+unit g_system;
+
+interface
+
+  uses Utils;
+
+  (* --- Utils --- *)
+  function sys_GetTicks (): Int64;
+  procedure sys_Delay (ms: Integer);
+
+  (* --- Graphics --- *)
+  function sys_GetDispalyModes (bpp: Integer): SSArray;
+  function sys_SetDisplayMode (w, h, bpp: Integer; fullscreen: Boolean): Boolean;
+  procedure sys_EnableVSync (yes: Boolean);
+  procedure sys_Repaint;
+
+  (* --- Input --- *)
+  function sys_HandleInput (): Boolean;
+  procedure sys_RequestQuit;
+
+  (* --- Init --- *)
+  procedure sys_Init;
+  procedure sys_Final;
+
+implementation
+
+  (* --------- Utils --------- *)
+
+  function sys_GetTicks (): Int64;
+  begin
+    Result := 0
+  end;
+
+  procedure sys_Delay (ms: Integer);
+  begin
+  end;
+
+  (* --------- Graphics --------- *)
+
+  procedure sys_Repaint;
+  begin
+  end;
+
+  procedure sys_EnableVSync (yes: Boolean);
+  begin
+  end;
+
+  function sys_GetDispalyModes (bpp: Integer): SSArray;
+  begin
+    SetLength(result, 0);
+  end;
+
+  function sys_SetDisplayMode (w, h, bpp: Integer; fullscreen: Boolean): Boolean;
+  begin
+    result := True
+  end;
+
+  (* --------- Input --------- *)
+
+  function sys_HandleInput (): Boolean;
+  begin
+    result := false
+  end;
+
+  procedure sys_RequestQuit;
+  begin
+  end;
+
+  (* --------- Init --------- *)
+
+  procedure sys_Init;
+  begin
+  end;
+
+  procedure sys_Final;
+  begin
+  end;
+
+end.

src/engine/e_graphics.pas
@@ -19,7 +19,11 @@ interface
 
 uses
   {$INCLUDE ../nogl/noGLuses.inc}
-  SysUtils, Classes, Math, e_log, e_texture, SDL2, MAPDEF, ImagingTypes, Imaging, ImagingUtility;
+  {$IFDEF USE_SDL2}
+    SDL2,
+  {$ENDIF}
+  SysUtils, Classes, Math, e_log, e_texture,
+  MAPDEF, ImagingTypes, Imaging, ImagingUtility;
 
 type
   TMirrorType=(None, Horizontal, Vertical);
@@ -128,8 +132,10 @@ procedure e_Clear(Mask: TGLbitfield; Red, Green, Blue: Single); overload;
 procedure e_Clear(); overload;
 procedure e_EndRender();
 
+{$IFDEF USE_SDL2}
 function e_GetGamma(win: PSDL_Window): Byte;
 procedure e_SetGamma(win: PSDL_Window;Gamma: Byte);
+{$ENDIF}
 
 procedure e_MakeScreenshot(st: TStream; Width, Height: Word);
 
@@ -1113,6 +1119,7 @@ begin
   glPopMatrix();
 end;
 
+{$IFDEF USE_SDL2}
 function e_GetGamma(win: PSDL_Window): Byte;
 var
   ramp: array [0..256*3-1] of Word;
@@ -1175,6 +1182,7 @@ begin
 
  SDL_SetWindowGammaRamp(win, @ramp[0], @ramp[256], @ramp[512]);
 end;
+{$ENDIF}
 
 function e_CharFont_Create(sp: ShortInt=0): DWORD;
 var

src/engine/e_input.pas
@@ -15,450 +15,12 @@
 {$INCLUDE ../shared/a_modes.inc}
 unit e_input;
 
-interface
-
-uses SysUtils, SDL2;
-
-const
-  e_MaxKbdKeys  = SDL_NUM_SCANCODES;
-  e_MaxJoys     = 4;
-  e_MaxJoyBtns  = 32;
-  e_MaxJoyAxes  = 8;
-  e_MaxJoyHats  = 8;
-  e_MaxVirtKeys = 48;
-
-  e_MaxJoyKeys = e_MaxJoyBtns + e_MaxJoyAxes*2 + e_MaxJoyHats*4;
-
-  e_MaxInputKeys = e_MaxKbdKeys + e_MaxJoys*e_MaxJoyKeys + e_MaxVirtKeys - 1;
-  // $$$..$$$ -  321 Keyboard buttons/keys
-  // $$$..$$$ - 4*32 Joystick buttons
-  // $$$..$$$ -  8*2 Joystick axes (- and +)
-  // $$$..$$$ -  4*4 Joystick hats (L U R D)
-  // $$$..$$$ -   48 Virtual buttons/keys
-
-  KBRD_END = e_MaxKbdKeys;
-  JOYK_BEG = KBRD_END;
-  JOYK_END = JOYK_BEG + e_MaxJoyBtns*e_MaxJoys;
-  JOYA_BEG = JOYK_END;
-  JOYA_END = JOYA_BEG + e_MaxJoyAxes*2*e_MaxJoys;
-  JOYH_BEG = JOYA_END;
-  JOYH_END = JOYH_BEG + e_MaxJoyHats*4*e_MaxJoys;
-  VIRT_BEG = JOYH_END;
-  VIRT_END = VIRT_BEG + e_MaxVirtKeys;
-
-  // these are apparently used in g_gui and g_game and elsewhere
-  IK_INVALID = 0;
-  IK_ESCAPE  = SDL_SCANCODE_ESCAPE;
-  IK_RETURN  = SDL_SCANCODE_RETURN;
-  IK_KPRETURN= SDL_SCANCODE_KP_ENTER;
-  IK_ENTER   = SDL_SCANCODE_RETURN;
-  IK_KPINSERT = SDL_SCANCODE_KP_0;
-  IK_UP      = SDL_SCANCODE_UP;
-  IK_KPUP    = SDL_SCANCODE_KP_8;
-  IK_DOWN    = SDL_SCANCODE_DOWN;
-  IK_KPDOWN  = SDL_SCANCODE_KP_2;
-  IK_LEFT    = SDL_SCANCODE_LEFT;
-  IK_KPLEFT  = SDL_SCANCODE_KP_4;
-  IK_RIGHT   = SDL_SCANCODE_RIGHT;
-  IK_KPRIGHT = SDL_SCANCODE_KP_6;
-  IK_DELETE  = SDL_SCANCODE_DELETE;
-  IK_HOME    = SDL_SCANCODE_HOME;
-  IK_KPHOME  = SDL_SCANCODE_KP_7;
-  IK_INSERT  = SDL_SCANCODE_INSERT;
-  IK_SPACE   = SDL_SCANCODE_SPACE;
-  IK_CONTROL = SDL_SCANCODE_LCTRL;
-  IK_SHIFT   = SDL_SCANCODE_LSHIFT;
-  IK_ALT     = SDL_SCANCODE_LALT;
-  IK_TAB     = SDL_SCANCODE_TAB;
-  IK_PAGEUP  = SDL_SCANCODE_PAGEUP;
-  IK_KPPAGEUP= SDL_SCANCODE_KP_9;
-  IK_PAGEDN  = SDL_SCANCODE_PAGEDOWN;
-  IK_KPPAGEDN= SDL_SCANCODE_KP_3;
-  IK_KP5     = SDL_SCANCODE_KP_5;
-  IK_NUMLOCK = SDL_SCANCODE_NUMLOCKCLEAR;
-  IK_KPDIVIDE= SDL_SCANCODE_KP_DIVIDE;
-  IK_KPMULTIPLE= SDL_SCANCODE_KP_MULTIPLY;
-  IK_KPMINUS = SDL_SCANCODE_KP_MINUS;
-  IK_KPPLUS  = SDL_SCANCODE_KP_PLUS;
-  IK_KPENTER = SDL_SCANCODE_KP_ENTER;
-  IK_KPDOT   = SDL_SCANCODE_KP_PERIOD;
-  IK_CAPSLOCK= SDL_SCANCODE_CAPSLOCK;
-  IK_RSHIFT  = SDL_SCANCODE_RSHIFT;
-  IK_CTRL    = SDL_SCANCODE_LCTRL;
-  IK_RCTRL   = SDL_SCANCODE_RCTRL;
-  IK_RALT    = SDL_SCANCODE_RALT;
-  IK_WIN     = SDL_SCANCODE_LGUI;
-  IK_RWIN    = SDL_SCANCODE_RGUI;
-  IK_MENU    = SDL_SCANCODE_MENU;
-  IK_PRINTSCR= SDL_SCANCODE_PRINTSCREEN;
-  IK_SCROLLLOCK= SDL_SCANCODE_SCROLLLOCK;
-  IK_LBRACKET= SDL_SCANCODE_LEFTBRACKET;
-  IK_RBRACKET= SDL_SCANCODE_RIGHTBRACKET;
-  IK_SEMICOLON= SDL_SCANCODE_SEMICOLON;
-  IK_QUOTE   = SDL_SCANCODE_APOSTROPHE;
-  IK_BACKSLASH= SDL_SCANCODE_BACKSLASH;
-  IK_SLASH   = SDL_SCANCODE_SLASH;
-  IK_COMMA   = SDL_SCANCODE_COMMA;
-  IK_DOT     = SDL_SCANCODE_PERIOD;
-  IK_EQUALS  = SDL_SCANCODE_EQUALS;
-  IK_0      = SDL_SCANCODE_0;
-  IK_1      = SDL_SCANCODE_1;
-  IK_2      = SDL_SCANCODE_2;
-  IK_3      = SDL_SCANCODE_3;
-  IK_4      = SDL_SCANCODE_4;
-  IK_5      = SDL_SCANCODE_5;
-  IK_6      = SDL_SCANCODE_6;
-  IK_7      = SDL_SCANCODE_7;
-  IK_8      = SDL_SCANCODE_8;
-  IK_9      = SDL_SCANCODE_9;
-  IK_F1      = SDL_SCANCODE_F1;
-  IK_F2      = SDL_SCANCODE_F2;
-  IK_F3      = SDL_SCANCODE_F3;
-  IK_F4      = SDL_SCANCODE_F4;
-  IK_F5      = SDL_SCANCODE_F5;
-  IK_F6      = SDL_SCANCODE_F6;
-  IK_F7      = SDL_SCANCODE_F7;
-  IK_F8      = SDL_SCANCODE_F8;
-  IK_F9      = SDL_SCANCODE_F9;
-  IK_F10     = SDL_SCANCODE_F10;
-  IK_F11     = SDL_SCANCODE_F11;
-  IK_F12     = SDL_SCANCODE_F12;
-  IK_END     = SDL_SCANCODE_END;
-  IK_KPEND   = SDL_SCANCODE_KP_1;
-  IK_BACKSPACE = SDL_SCANCODE_BACKSPACE;
-  IK_BACKQUOTE = SDL_SCANCODE_GRAVE;
-  IK_GRAVE     = SDL_SCANCODE_GRAVE;
-  IK_PAUSE   = SDL_SCANCODE_PAUSE;
-  IK_Y       = SDL_SCANCODE_Y;
-  IK_N       = SDL_SCANCODE_N;
-  IK_W       = SDL_SCANCODE_W;
-  IK_A       = SDL_SCANCODE_A;
-  IK_S       = SDL_SCANCODE_S;
-  IK_D       = SDL_SCANCODE_D;
-  IK_Q       = SDL_SCANCODE_Q;
-  IK_E       = SDL_SCANCODE_E;
-  IK_H       = SDL_SCANCODE_H;
-  IK_J       = SDL_SCANCODE_J;
-  IK_T       = SDL_SCANCODE_T;
-  IK_Z       = SDL_SCANCODE_Z;
-  IK_MINUS   = SDL_SCANCODE_MINUS;
-  // TODO: think of something better than this shit
-  IK_LASTKEY = SDL_NUM_SCANCODES-1;
-
-  VK_FIRSTKEY = e_MaxKbdKeys + e_MaxJoys*e_MaxJoyKeys;
-  VK_LEFT     = VK_FIRSTKEY + 0;
-  VK_RIGHT    = VK_FIRSTKEY + 1;
-  VK_UP       = VK_FIRSTKEY + 2;
-  VK_DOWN     = VK_FIRSTKEY + 3;
-  VK_FIRE     = VK_FIRSTKEY + 4;
-  VK_OPEN     = VK_FIRSTKEY + 5;
-  VK_JUMP     = VK_FIRSTKEY + 6;
-  VK_CHAT     = VK_FIRSTKEY + 7;
-  VK_ESCAPE   = VK_FIRSTKEY + 8;
-  VK_0        = VK_FIRSTKEY + 9;
-  VK_1        = VK_FIRSTKEY + 10;
-  VK_2        = VK_FIRSTKEY + 11;
-  VK_3        = VK_FIRSTKEY + 12;
-  VK_4        = VK_FIRSTKEY + 13;
-  VK_5        = VK_FIRSTKEY + 14;
-  VK_6        = VK_FIRSTKEY + 15;
-  VK_7        = VK_FIRSTKEY + 16;
-  VK_8        = VK_FIRSTKEY + 17;
-  VK_9        = VK_FIRSTKEY + 18;
-  VK_A        = VK_FIRSTKEY + 19;
-  VK_B        = VK_FIRSTKEY + 20;
-  VK_C        = VK_FIRSTKEY + 21;
-  VK_D        = VK_FIRSTKEY + 22;
-  VK_E        = VK_FIRSTKEY + 23;
-  VK_F        = VK_FIRSTKEY + 24;
-  VK_CONSOLE  = VK_FIRSTKEY + 25;
-  VK_STATUS   = VK_FIRSTKEY + 26;
-  VK_TEAM     = VK_FIRSTKEY + 27;
-  VK_PREV     = VK_FIRSTKEY + 28;
-  VK_NEXT     = VK_FIRSTKEY + 29;
-  VK_STRAFE   = VK_FIRSTKEY + 30;
-  VK_LSTRAFE  = VK_FIRSTKEY + 31;
-  VK_RSTRAFE  = VK_FIRSTKEY + 32;
-  VK_PRINTSCR = VK_FIRSTKEY + 33;
-  VK_SHOWKBD  = VK_FIRSTKEY + 34;
-  VK_HIDEKBD  = VK_FIRSTKEY + 35;
-  VK_LASTKEY  = e_MaxKbdKeys + e_MaxJoys*e_MaxJoyKeys + e_MaxVirtKeys - 1;
-
-  AX_MINUS  = 0;
-  AX_PLUS   = 1;
-  HAT_LEFT  = 0;
-  HAT_UP    = 1;
-  HAT_RIGHT = 2;
-  HAT_DOWN  = 3;
-
-  JOY0_ATTACK = JOYK_BEG + 0*e_MaxJoyBtns + 0;
-  JOY1_ATTACK = JOYK_BEG + 1*e_MaxJoyBtns + 0;
-  JOY2_ATTACK = JOYK_BEG + 2*e_MaxJoyBtns + 0;
-  JOY3_ATTACK = JOYK_BEG + 3*e_MaxJoyBtns + 0;
-  JOY0_NEXT = JOYK_BEG + 0*e_MaxJoyBtns + 1;
-  JOY1_NEXT = JOYK_BEG + 1*e_MaxJoyBtns + 1;
-  JOY2_NEXT = JOYK_BEG + 2*e_MaxJoyBtns + 1;
-  JOY3_NEXT = JOYK_BEG + 3*e_MaxJoyBtns + 1;
-  JOY0_JUMP = JOYK_BEG + 0*e_MaxJoyBtns + 2;
-  JOY1_JUMP = JOYK_BEG + 1*e_MaxJoyBtns + 2;
-  JOY2_JUMP = JOYK_BEG + 2*e_MaxJoyBtns + 2;
-  JOY3_JUMP = JOYK_BEG + 3*e_MaxJoyBtns + 2;
-  JOY0_ACTIVATE = JOYK_BEG + 0*e_MaxJoyBtns + 3;
-  JOY1_ACTIVATE = JOYK_BEG + 1*e_MaxJoyBtns + 3;
-  JOY2_ACTIVATE = JOYK_BEG + 2*e_MaxJoyBtns + 3;
-  JOY3_ACTIVATE = JOYK_BEG + 3*e_MaxJoyBtns + 3;
-  JOY0_PREV = JOYK_BEG + 0*e_MaxJoyBtns + 4;
-  JOY1_PREV = JOYK_BEG + 1*e_MaxJoyBtns + 4;
-  JOY2_PREV = JOYK_BEG + 2*e_MaxJoyBtns + 4;
-  JOY3_PREV = JOYK_BEG + 3*e_MaxJoyBtns + 4;
-
-  JOY0_LEFT = JOYH_BEG + 0*e_MaxJoyHats*4 + 0*4 + HAT_LEFT;
-  JOY1_LEFT = JOYH_BEG + 1*e_MaxJoyHats*4 + 0*4 + HAT_LEFT;
-  JOY2_LEFT = JOYH_BEG + 2*e_MaxJoyHats*4 + 0*4 + HAT_LEFT;
-  JOY3_LEFT = JOYH_BEG + 3*e_MaxJoyHats*4 + 0*4 + HAT_LEFT;
-  JOY0_RIGHT = JOYH_BEG + 0*e_MaxJoyHats*4 + 0*4 + HAT_RIGHT;
-  JOY1_RIGHT = JOYH_BEG + 1*e_MaxJoyHats*4 + 0*4 + HAT_RIGHT;
-  JOY2_RIGHT = JOYH_BEG + 2*e_MaxJoyHats*4 + 0*4 + HAT_RIGHT;
-  JOY3_RIGHT = JOYH_BEG + 3*e_MaxJoyHats*4 + 0*4 + HAT_RIGHT;
-  JOY0_UP = JOYH_BEG + 0*e_MaxJoyHats*4 + 0*4 + HAT_UP;
-  JOY1_UP = JOYH_BEG + 1*e_MaxJoyHats*4 + 0*4 + HAT_UP;
-  JOY2_UP = JOYH_BEG + 2*e_MaxJoyHats*4 + 0*4 + HAT_UP;
-  JOY3_UP = JOYH_BEG + 3*e_MaxJoyHats*4 + 0*4 + HAT_UP;
-  JOY0_DOWN = JOYH_BEG + 0*e_MaxJoyHats*4 + 0*4 + HAT_DOWN;
-  JOY1_DOWN = JOYH_BEG + 1*e_MaxJoyHats*4 + 0*4 + HAT_DOWN;
-  JOY2_DOWN = JOYH_BEG + 2*e_MaxJoyHats*4 + 0*4 + HAT_DOWN;
-  JOY3_DOWN = JOYH_BEG + 3*e_MaxJoyHats*4 + 0*4 + HAT_DOWN;
-
-function  e_InitInput: Boolean;
-procedure e_ReleaseInput;
-procedure e_UnpressAllKeys;
-procedure e_KeyUpDown (key: Integer; down: Boolean);
-
-function  e_KeyPressed (key: Integer): Boolean;
-function  e_AnyKeyPressed: Boolean;
-function  e_GetFirstKeyPressed: Integer;
-function  e_HasJoysticks: Boolean;
-
-function  e_JoyButtonToKey (id, btn: Integer): Integer;
-function  e_JoyAxisToKey (id, ax, dir: Integer): Integer;
-function  e_JoyHatToKey (id, hat, dir: Integer): Integer;
-
-var
-  e_JoystickAvailable: array [0..e_MaxJoys - 1] of Boolean;
-  e_JoystickDeadzones: array [0..e_MaxJoys - 1] of Integer = (8192, 8192, 8192, 8192);
-  e_KeyNames: array [0..e_MaxInputKeys] of String;
-
-implementation
-
-var
-  InputBuffer: array [0..e_MaxInputKeys - 1] of Boolean;
-
-procedure e_UnpressAllKeys;
-  var i: Integer;
-begin
-  for i := 0 to High(InputBuffer) do
-    InputBuffer[i] := False
-end;
-
-procedure e_KeyUpDown (key: Integer; down: Boolean);
-begin
-  ASSERT(key >= 0);
-  ASSERT(key < e_MaxInputKeys);
-  if key > 0 then
-    InputBuffer[key] := down
-end;
-
-procedure GenerateKeyNames;
-  var i, j, k: Integer;
-begin
-  // keyboard key names
-  e_KeyNames[IK_0] := '0';
-  e_KeyNames[IK_1] := '1';
-  e_KeyNames[IK_2] := '2';
-  e_KeyNames[IK_3] := '3';
-  e_KeyNames[IK_4] := '4';
-  e_KeyNames[IK_5] := '5';
-  e_KeyNames[IK_6] := '6';
-  e_KeyNames[IK_7] := '7';
-  e_KeyNames[IK_8] := '8';
-  e_KeyNames[IK_9] := '9';
-
-  for i := IK_A to IK_Z do
-    e_KeyNames[i] := '' + chr(ord('a') + (i - IK_a));
-
-  e_KeyNames[IK_ESCAPE] := 'ESCAPE';
-  e_KeyNames[IK_ENTER] := 'ENTER';
-  e_KeyNames[IK_TAB] := 'TAB';
-  e_KeyNames[IK_BACKSPACE] := 'BACKSPACE';
-  e_KeyNames[IK_SPACE] := 'SPACE';
-  e_KeyNames[IK_UP] := 'UP';
-  e_KeyNames[IK_LEFT] := 'LEFT';
-  e_KeyNames[IK_RIGHT] := 'RIGHT';
-  e_KeyNames[IK_DOWN] := 'DOWN';
-  e_KeyNames[IK_INSERT] := 'INSERT';
-  e_KeyNames[IK_DELETE] := 'DELETE';
-  e_KeyNames[IK_HOME] := 'HOME';
-  e_KeyNames[IK_END] := 'END';
-  e_KeyNames[IK_PAGEUP] := 'PGUP';
-  e_KeyNames[IK_PAGEDN] := 'PGDOWN';
-  e_KeyNames[IK_KPINSERT] := 'PAD0';
-  e_KeyNames[IK_KPEND] := 'PAD1';
-  e_KeyNames[IK_KPDOWN] := 'PAD2';
-  e_KeyNames[IK_KPPAGEDN] := 'PAD3';
-  e_KeyNames[IK_KPLEFT] := 'PAD4';
-  e_KeyNames[IK_KP5] := 'PAD5';
-  e_KeyNames[IK_KPRIGHT] := 'PAD6';
-  e_KeyNames[IK_KPHOME] := 'PAD7';
-  e_KeyNames[IK_KPUP] := 'PAD8';
-  e_KeyNames[IK_KPPAGEUP] := 'PAD9';
-  e_KeyNames[IK_NUMLOCK] := 'NUM';
-  e_KeyNames[IK_KPDIVIDE] := 'PAD/';
-  e_KeyNames[IK_KPMULTIPLE] := 'PAD*';
-  e_KeyNames[IK_KPMINUS] := 'PAD-';
-  e_KeyNames[IK_KPPLUS] := 'PAD+';
-  e_KeyNames[IK_KPENTER] := 'PADENTER';
-  e_KeyNames[IK_KPDOT] := 'PAD.';
-  e_KeyNames[IK_CAPSLOCK] := 'CAPS';
-  e_KeyNames[IK_BACKQUOTE] := 'BACKQUOTE';
-  e_KeyNames[IK_F1] := 'F1';
-  e_KeyNames[IK_F2] := 'F2';
-  e_KeyNames[IK_F3] := 'F3';
-  e_KeyNames[IK_F4] := 'F4';
-  e_KeyNames[IK_F5] := 'F5';
-  e_KeyNames[IK_F6] := 'F6';
-  e_KeyNames[IK_F7] := 'F7';
-  e_KeyNames[IK_F8] := 'F8';
-  e_KeyNames[IK_F9] := 'F9';
-  e_KeyNames[IK_F10] := 'F10';
-  e_KeyNames[IK_F11] := 'F11';
-  e_KeyNames[IK_F12] := 'F12';
-  e_KeyNames[IK_SHIFT] := 'LSHIFT';
-  e_KeyNames[IK_RSHIFT] := 'RSHIFT';
-  e_KeyNames[IK_CTRL] := 'LCTRL';
-  e_KeyNames[IK_RCTRL] := 'RCTRL';
-  e_KeyNames[IK_ALT] := 'LALT';
-  e_KeyNames[IK_RALT] := 'RALT';
-  e_KeyNames[IK_WIN] := 'LWIN';
-  e_KeyNames[IK_RWIN] := 'RWIN';
-  e_KeyNames[IK_MENU] := 'MENU';
-  e_KeyNames[IK_PRINTSCR] := 'PSCRN';
-  e_KeyNames[IK_SCROLLLOCK] := 'SCROLL';
-  e_KeyNames[IK_PAUSE] := 'PAUSE';
-  e_KeyNames[IK_LBRACKET] := '[';
-  e_KeyNames[IK_RBRACKET] := ']';
-  e_KeyNames[IK_SEMICOLON] := ';';
-  e_KeyNames[IK_QUOTE] := '''';
-  e_KeyNames[IK_BACKSLASH] := '\';
-  e_KeyNames[IK_SLASH] := '/';
-  e_KeyNames[IK_COMMA] := ',';
-  e_KeyNames[IK_DOT] := '.';
-  e_KeyNames[IK_MINUS] := '-';
-  e_KeyNames[IK_EQUALS] := '=';
-
-  // joysticks
-  for j := 0 to e_MaxJoys-1 do
-  begin
-    k := JOYK_BEG + j * e_MaxJoyBtns;
-    // buttons
-    for i := 0 to e_MaxJoyBtns-1 do
-      e_KeyNames[k + i] := Format('JOY%dB%d', [j, i]);
-    k := JOYA_BEG + j * e_MaxJoyAxes * 2;
-    // axes
-    for i := 0 to e_MaxJoyAxes-1 do
-    begin
-      e_KeyNames[k + i*2    ] := Format('JOY%dA%d+', [j, i]);
-      e_KeyNames[k + i*2 + 1] := Format('JOY%dA%d-', [j, i]);
-    end;
-    k := JOYH_BEG + j * e_MaxJoyHats * 4;
-    // hats
-    for i := 0 to e_MaxJoyHats-1 do
-    begin
-      e_KeyNames[k + i*4    ] := Format('JOY%dD%dL', [j, i]);
-      e_KeyNames[k + i*4 + 1] := Format('JOY%dD%dU', [j, i]);
-      e_KeyNames[k + i*4 + 2] := Format('JOY%dD%dR', [j, i]);
-      e_KeyNames[k + i*4 + 3] := Format('JOY%dD%dD', [j, i]);
-    end;
-  end;
-
-  // vitrual keys
-  for i := 0 to e_MaxVirtKeys-1 do
-    e_KeyNames[VIRT_BEG + i] := 'VIRTUAL' + IntToStr(i);
-end;
-
-function e_HasJoysticks: Boolean;
-  var i: Integer;
-begin
-  i := 0;
-  while (i < e_MaxJoys) and (e_JoystickAvailable[i] = False) do inc(i);
-  result := i < e_MaxJoys
-end;
-
-function e_InitInput: Boolean;
-  var i: Integer;
-begin
-  for i := 0 to e_MaxJoys - 1 do
-    e_JoystickAvailable[i] := False;
-  GenerateKeyNames;
-  result := True
-end;
-
-procedure e_ReleaseInput;
-  var i: Integer;
-begin
-  for i := 0 to e_MaxJoys - 1 do
-    e_JoystickAvailable[i] := False
-end;
-
-function e_KeyPressed (key: Integer): Boolean;
-begin
-  ASSERT(key >= 0);
-  ASSERT(key < e_MaxInputKeys);
-  result := InputBuffer[key]
-end;
-
-function e_AnyKeyPressed: Boolean;
-begin
-  result := e_GetFirstKeyPressed <> IK_INVALID;
-end;
-
-function e_GetFirstKeyPressed: Integer;
-  var i: Integer;
-begin
-  i := 1;
-  while (i < e_MaxInputKeys) and (InputBuffer[i] = False) do inc(i);
-  if i < e_MaxInputKeys then
-    result := i
-  else
-    result := IK_INVALID
-end;
-
-function e_JoyButtonToKey (id, btn: Integer): Integer;
-begin
-  ASSERT(id >= 0);
-  ASSERT(id < e_MaxJoys);
-  ASSERT(btn >= 0);
-  ASSERT(btn < e_MaxJoyBtns);
-  result := JOYK_BEG + id*e_MaxJoyBtns + btn
-end;
-
-function e_JoyAxisToKey (id, ax, dir: Integer): Integer;
-begin
-  ASSERT(id >= 0);
-  ASSERT(id < e_MaxJoys);
-  ASSERT(ax >= 0);
-  ASSERT(ax < e_MaxJoyAxes);
-  ASSERT(dir in [AX_MINUS, AX_PLUS]);
-  result := JOYA_BEG + id*e_MaxJoyAxes*2 + ax*2 + dir
-end;
-
-function e_JoyHatToKey (id, hat, dir: Integer): Integer;
-begin
-  ASSERT(id >= 0);
-  ASSERT(id < e_MaxJoys);
-  ASSERT(hat >= 0);
-  ASSERT(hat < e_MaxJoyHats);
-  ASSERT(dir in [HAT_LEFT, HAT_UP, HAT_RIGHT, HAT_DOWN]);
-  result := JOYH_BEG + id*e_MaxJoyHats*4 + hat*4 + dir
-end;
+{$IF DEFINED(USE_SYSSTUB) OR DEFINED(USE_SDL)}
+  {$I e_input_stub.inc}
+{$ELSEIF DEFINED(USE_SDL2)}
+  {$I e_input_sdl2.inc}
+{$ELSE}
+  {$ERROR e_input driver not implemented?}
+{$ENDIF}
 
 end.

commit e84299fd3f1bf3f58e7aeddc017d2b6dc835f676

Game: Fixed missing check for the availability of stencil buffer This fixes experimental dynamic light effects ('light_enabled 1' and 'light_player_halo') which were broken by 0e101bd452c40da601236aaa2dd4106be47ddce1. It's worth noting that they even work on my laptop's integrated video if I just set 'gwin_has_stencil' to True, even though the driver says it doesn't support stencils. The discrete GPU on the same laptop reports it correctly.

src/game/sdl/g_system.pas
@@ -78,12 +78,22 @@ implementation
   (* --------- Graphics --------- *)
 
   function LoadGL: Boolean;
+  {$IFNDEF NOGL_INIT}
+  var
+    ltmp: Integer;
+  {$ENDIF}
   begin
     result := true;
     {$IFDEF NOGL_INIT}
     nogl_Init;
     if glRenderToFBO and (not nogl_ExtensionSupported('GL_OES_framebuffer_object')) then
     {$ELSE}
+    if SDL_GL_GetAttribute(SDL_GL_STENCIL_SIZE, @ltmp) = 0 then
+    begin
+      e_LogWritefln('stencil buffer size: %s', [ltmp]);
+      gwin_has_stencil := (ltmp > 0);
+    end;
+
     if glRenderToFBO and (not Load_GL_ARB_framebuffer_object) then
     {$ENDIF}
     begin

src/game/sdl2/g_system.pas
@@ -75,12 +75,22 @@ implementation
   (* --------- Graphics --------- *)
 
   function LoadGL: Boolean;
+  {$IFNDEF NOGL_INIT}
+  var
+    ltmp: Integer;
+  {$ENDIF}
   begin
     result := true;
     {$IFDEF NOGL_INIT}
     nogl_Init;
     if glRenderToFBO and (not nogl_ExtensionSupported('GL_OES_framebuffer_object')) then
     {$ELSE}
+    if SDL_GL_GetAttribute(SDL_GL_STENCIL_SIZE, @ltmp) = 0 then
+    begin
+      e_LogWritefln('stencil buffer size: %s', [ltmp]);
+      gwin_has_stencil := (ltmp > 0);
+    end;
+
     if glRenderToFBO and (not Load_GL_ARB_framebuffer_object) then
     {$ENDIF}
     begin