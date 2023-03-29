db32772f4f7f51c24c881ad590e69bc3069dc61a
DeaDDooMER <deaddoomer@deadsoftware.ru>
2023-03-29 17:51:50 +0300

    options: add forgotten file

---
+++ src/editor/g_options.pas
@@ -0,0 +1,14 @@
+unit g_options;
+
+  {$INCLUDE ../shared/a_modes.inc}
+
+interface
+
+  var
+    CfgFileName: AnsiString;
+    GameWad: AnsiString;
+    EditorWad: AnsiString;
+
+implementation
+
+end.
