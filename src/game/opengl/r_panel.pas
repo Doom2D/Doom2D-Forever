fl0atingzero/d2df-sdl

render: separate animation drawing from game code

src/game/opengl/r_panel.pas
@@ -27,7 +27,7 @@ implementation
   uses
     {$INCLUDE ../nogl/noGLuses.inc}
     SysUtils, Classes, Math,
-    r_graphics, g_options,
+    r_graphics, g_options, r_animations,
     g_base, g_basic, g_textures
   ;
 
@@ -46,7 +46,7 @@ implementation
             Exit;
           for xx := 0 to Width div TextureWidth - 1 do
             for yy := 0 to Height div TextureHeight - 1 do
-              TextureIDs[FCurTexture].AnTex.Draw(X + xx * TextureWidth, Y + yy * TextureHeight, TMirrorType.None);
+              r_Animation_Draw(TextureIDs[FCurTexture].AnTex, X + xx * TextureWidth, Y + yy * TextureHeight, TMirrorType.None);
         end
         else
         begin

Doom2D/Doom2D-Forever

src/game/opengl/r_panel.pas
@@ -27,7 +27,7 @@ implementation
   uses
     {$INCLUDE ../nogl/noGLuses.inc}
     SysUtils, Classes, Math, utils,
-    r_graphics, g_options,
+    r_graphics, g_options, r_animations,
     g_base, g_basic, g_textures, g_game
   ;
 
@@ -65,7 +65,7 @@ implementation
             Exit;
           for xx := 0 to tw div TextureWidth - 1 do
             for yy := 0 to th div TextureHeight - 1 do
-              TextureIDs[FCurTexture].AnTex.Draw(tx + xx * TextureWidth, ty + yy * TextureHeight, TMirrorType.None);
+              r_Animation_Draw(TextureIDs[FCurTexture].AnTex, tx + xx * TextureWidth, ty + yy * TextureHeight, TMirrorType.None);
         end
         else
         begin