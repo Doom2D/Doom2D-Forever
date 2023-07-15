Doom2D/Doom2D-Forever

commit 414f2873efa0cce84499f64774db7000e6268971

allow resources in non current directory (warning: res downloader are broken)

src/game/g_player.pas
@@ -613,7 +613,7 @@ uses
   g_holmes,
 {$ENDIF}
   e_log, g_map, g_items, g_console, g_gfx, Math,
-  g_options, g_triggers, g_menu, g_game, g_grid,
+  g_options, g_triggers, g_menu, g_game, g_grid, e_res,
   wadreader, g_main, g_monsters, CONFIG, g_language,
   g_net, g_netmsg, g_window,
   utils, xstreams;
@@ -1254,14 +1254,16 @@ var
   a, b: Integer;
   config: TConfig;
   sa: SSArray;
+  path: AnsiString;
 begin
   BotNames := nil;
 
-  if not FileExists(DataDir + BOTNAMES_FILENAME) then
+  path := BOTNAMES_FILENAME;
+  if e_FindResource(DataDirs, path) = false then
     Exit;
 
 // Читаем возможные имена ботов из файла:
-  AssignFile(F, DataDir + BOTNAMES_FILENAME);
+  AssignFile(F, path);
   Reset(F);
 
   while not EOF(F) do
@@ -1282,7 +1284,7 @@ begin
   g_Bot_MixNames();
 
 // Читаем файл с параметрами ботов:
-  config := TConfig.CreateFile(DataDir + BOTLIST_FILENAME);
+  config := TConfig.CreateFile(path);
   BotList := nil;
   a := 0;
 

src/engine/e_res.pas
@@ -1,0 +1,259 @@
+(* Copyright (C)  Doom 2D: Forever Developers
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License ONLY.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *)
+{$I ../shared/a_modes.inc}
+unit e_res;
+
+interface
+
+  uses SysUtils, Utils, Classes;
+
+  var
+    debug_e_res: Boolean;
+
+  {-------------------------------------------}
+  {--- insert separator beetwin a and b    ---}
+  {--- result are correct if (a or b) = '' ---}
+  {--- - - - - - - - - - - - - - - - - - - ---}
+  function e_CatPath (a, b: AnsiString): AnsiString;
+
+  {--- remove last entry from path ---}
+  function e_UpperDir (path: AnsiString): AnsiString;
+
+  {--- not absolute and have no relative dirs ---}
+  function e_IsValidResourceName (name: AnsiString): Boolean;
+
+  {-----------------------------------------------------------------------}
+  {--- try to open/create file in one dir from `dirs` in reverse order ---}
+  {--- e_OpenResourceRW tries to create if not exists                  ---}
+  {--- create dirs if not exists                                       ---}
+  {--- result <> nil, throws exceptions on errors                      ---}
+  {--- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ---}
+  function e_CreateResource (dirs: SSArray; name: AnsiString): TStream;
+  function e_OpenResourceRO (dirs: SSArray; name: AnsiString): TStream;
+  function e_OpenResourceRW (dirs: SSArray; name: AnsiString): TStream;
+
+  {--- same as shared/utils ---}
+  function e_FindResource (dirs: SSArray; var name: AnsiString; nameIsDir: Boolean = false): Boolean;
+  function e_FindWad (dirs: SSArray; name: AnsiString): AnsiString;
+
+  {--- append dirs to 'path.wad:\file'. if disk is void, append defWad ---}
+  function e_GetResourcePath (dirs: SSArray; path: AnsiString; defWad: AnsiString): AnsiString;
+
+  {--- same as SysUtils.FinFirst ---}
+  function e_FindFirst (dirs: SSArray; name: AnsiString; attr: LongInt; out Rslt: TRawbyteSearchRec): LongInt;
+
+  {--- try to create directory from list, throws if no one directory created ---}
+  function e_GetDir (dirs: SSArray): AnsiString;
+
+implementation
+
+  uses WadReader, e_log;
+
+  type
+    SpawnProc = function (pathname: AnsiString): Tstream;
+
+  function e_UpperDir (path: AnsiString): AnsiString;
+    var i: Integer;
+  begin
+    i := High(path);
+    while (i >= 1) and (path[i] <> '/') and (path[i] <> '\') do Dec(i);
+    result := Copy(path, 1, i)
+  end;
+
+  function HasRelativeDirs (name: AnsiString): Boolean;
+    var i: Integer; ch: Char;
+  begin
+    i := 1;
+    result := false;
+    while (result = false) and (name[i] <> #0) do
+    begin
+      ch := name[i];
+      if (ch = '/') or (ch = '\') then
+      begin
+        Inc(i);
+        if name[i] = '.' then
+        begin
+          Inc(i);
+          if name[i] = '.' then
+          begin
+            Inc(i);
+            ch := name[i];
+            result := (ch = #0) or (ch = '/') or (ch = '\')
+          end
+        end
+      end
+      else
+      begin
+        Inc(i)
+      end
+    end
+  end;
+
+  function HasAbsoluteDirs (name: AnsiString): Boolean;
+  begin
+    result := (name = '') or (name[1] = '/') or (name[1] = '\')
+  end;
+
+  function e_IsValidResourceName (name: AnsiString): Boolean;
+  begin
+    result := (HasAbsoluteDirs(name) = false) and (HasRelativeDirs(name) = false)
+  end;
+
+  function SpawnStream (dirs: SSArray; name: AnsiString; p: SpawnProc; createNewDir: Boolean): TStream;
+    var i: Integer;
+  begin
+    result := nil;
+    assert(dirs <> nil);
+    assert(e_IsValidResourceName(name));
+    i := High(dirs);
+    while (i >= 0) and (result = nil) do
+    begin
+      try
+        if debug_e_res then
+          e_LogWritefln('  %s', [dirs[i]]);
+        if (createNewDir = false) or (ForceDirectories(dirs[i]) = true) then
+          result := p(e_CatPath(dirs[i], name))
+      finally
+        Dec(i)
+      end
+    end
+  end;
+
+  function e_CreateResource (dirs: SSArray; name: AnsiString): TStream;
+  begin
+    if debug_e_res then
+      e_LogWritefln('e_CreateResource %s', [name]);
+    result := SpawnStream(dirs, name, @createDiskFile, true);
+    if result = nil then
+      raise Exception.Create('can''t create resource "' + name + '"');
+  end;
+
+  function e_OpenResourceRO (dirs: SSArray; name: AnsiString): TStream;
+  begin
+    if debug_e_res then
+      e_LogWritefln('e_OpenResourceRO %s', [name]);
+    result := SpawnStream(dirs, name, @openDiskFileRO, false);
+    if result = nil then
+      raise EFileNotFoundException.Create('can''t open resource "' + name + '"')
+  end;
+
+  function e_OpenResourceRW (dirs: SSArray; name: AnsiString): TStream;
+  begin
+    if debug_e_res then
+      e_LogWritefln('e_OpenResourceRW %s', [name]);
+    result := SpawnStream(dirs, name, @openDiskFileRW, true);
+    if result = nil then
+      raise Exception.Create('can''t create resource "' + name + '"')
+  end;
+
+  function e_CatPath (a, b: AnsiString): AnsiString;
+  begin
+    if a = '' then
+      result := b
+    else if b = '' then
+      result := a
+    else
+      result := a + '/' + b
+  end;
+
+  function e_FindResource (dirs: SSArray; var name: AnsiString; nameIsDir: Boolean = false): Boolean;
+    var i: Integer; dir: AnsiString;
+  begin
+    if debug_e_res then
+      e_LogWritefln('e_FindResource %s (%s)', [name, nameIsDir]);
+    result := false;
+    assert(dirs <> nil);
+    assert(e_IsValidResourceName(name));
+    i := High(dirs); dir := name;
+    while (i >= 0) and (result = false) do
+    begin
+      dir := e_CatPath(dirs[i], name);
+      result := findFileCI(dir, nameIsDir);
+      if debug_e_res then
+        e_LogWritefln('  %s -> %s', [dir, result]);
+      Dec(i)
+    end;
+    if result = true then
+      name := dir;
+    if debug_e_res then
+      e_LogWritefln('  result = %s (%s)', [name, result]);
+  end;
+
+  function e_FindWad (dirs: SSArray; name: AnsiString): AnsiString;
+    var i: Integer;
+  begin
+    if debug_e_res then
+      e_LogWritefln('e_FindWad "%s"', [name]);
+    result := '';
+    assert(dirs <> nil);
+    assert(e_IsValidResourceName(name));
+    i := High(dirs);
+    while (i >= 0) and (result = '') do
+    begin
+      result := findDiskWad(dirs[i] + DirectorySeparator + name);
+      if debug_e_res then
+        e_LogWritefln('  %s -> %s', [dirs[i] + DirectorySeparator + name, result]);
+      Dec(i)
+    end
+  end;
+
+  function e_GetResourcePath (dirs: SSArray; path: AnsiString; defWad: AnsiString): AnsiString;
+    var diskName, fileName: AnsiString;
+  begin
+    if debug_e_res then
+      e_LogWritefln('e_GetResourcePath0 %s (%s)', [path, defWad]);
+    assert(dirs <> nil);
+    assert(path <> '');
+    assert(defWad <> '');
+    diskName := g_ExtractWadName(path);
+    fileName := g_ExtractFilePathName(path);
+    if diskName = '' then diskName := defWad else diskName := e_FindWad(dirs, diskName);
+    assert(diskName <> '', 'oh fuck, wad "' + diskName + '" not founded');
+    result := diskName + ':\' + fileName;
+    if debug_e_res then
+      e_LogWritefln('  this>>> %s', [result]);
+  end;
+
+  function e_FindFirst (dirs: SSArray; name: AnsiString; attr: LongInt; out Rslt: TRawbyteSearchRec): LongInt;
+    var i: Integer; dir: AnsiString;
+  begin
+    if debug_e_res then
+      e_LogWritefln('e_FindFirst %s', [name]);
+    assert(dirs <> nil);
+    assert(e_IsValidResourceName(name));
+    i := High(dirs); result := -1;
+    while (i >= 0) and (result <> 0) do
+    begin
+      dir := dirs[i] + DirectorySeparator + name;
+      result := FindFirst(dir, attr, Rslt);
+      if debug_e_res then
+        e_LogWritefln('  %s: %s -- %s', [i, dir, result]);
+      Dec(i); 
+    end
+  end;
+
+  function e_GetDir (dirs: SSArray): AnsiString;
+    var i: Integer;
+  begin
+    result := '';
+    i := High(dirs);
+    while (i >= 0) and (ForceDirectories(dirs[i]) = false) do Dec(i);
+    if i >= 0 then
+      result := dirs[i]
+    else
+      raise Exception.Create('unable to create directory')
+  end;
+
+end.

src/game/Doom2DF.lpr
@@ -101,6 +101,7 @@ uses
   e_sound in '../engine/e_sound.pas',
   e_texture in '../engine/e_texture.pas',
   e_msg in '../engine/e_msg.pas',
+  e_res in '../engine/e_res.pas',
   utils in '../shared/utils.pas',
   xstreams in '../shared/xstreams.pas',
   sfs in '../sfs/sfs.pas',
@@ -240,15 +241,6 @@ begin
       end;
     end;
     Inc(f)
-  end;
-
-  if LogFileName = '' then
-  begin
-{$IFDEF HEADLESS}
-    LogFileName := 'Doom2DF_H.log';
-{$ELSE}
-    LogFileName := 'Doom2DF.log';
-{$ENDIF}
   end;
 
   if noct then

src/game/g_console.pas
@@ -86,9 +86,13 @@ implementation
 
 uses
   g_textures, g_main, e_graphics, e_input, g_game,
-  SysUtils, g_basic, g_options, Math, g_touch,
+  SysUtils, g_basic, g_options, Math, g_touch, e_res,
   g_menu, g_gui, g_language, g_net, g_netmsg, e_log, conbuf;
 
+const
+  configScript = 'dfconfig.cfg';
+  autoexecScript = 'autoexec.cfg';
+  configComment = 'generated by doom2d, do not modify';
 
 type
   PCommand = ^TCommand;
@@ -580,7 +584,7 @@ begin
   begin
     // exec <filename>
     if Length(p) = 2 then
-      g_Console_ReadConfig(GameDir + '/' + p[1])
+      g_Console_ReadConfig(p[1])
     else
       g_Console_Add('exec <script file>');
   end;
@@ -589,9 +593,14 @@ begin
   begin
     // writeconfig <filename>
     if Length(p) = 2 then
-      g_Console_WriteConfig(GameDir + '/' + p[1])
+    begin
+      s := e_GetDir(ConfigDirs);
+      g_Console_WriteConfig(e_CatPath(s, p[1]))
+    end
     else
-      g_Console_Add('writeconfig <file>');
+    begin
+      g_Console_Add('writeconfig <file>')
+    end
   end;
 
   if (cmd = 'ver') or (cmd = 'version') then
@@ -1015,8 +1024,8 @@ begin
   WhitelistCommand('g_timelimit');
 
   g_Console_ResetBinds;
-  g_Console_ReadConfig(GameDir + '/dfconfig.cfg');
-  g_Console_ReadConfig(GameDir + '/autoexec.cfg');
+  g_Console_ReadConfig(configScript);
+  g_Console_ReadConfig(autoexecScript);
   gParsingBinds := False;
 end;
 
@@ -1881,8 +1890,10 @@ end;
 procedure g_Console_ReadConfig (filename: String);
   var f: TextFile; s: AnsiString; i, len: Integer;
 begin
-  if FileExists(filename) then
+  e_LogWritefln('g_Console_ReadConfig (1) "%s"', [filename]);
+  if e_FindResource(ConfigDirs, filename, false) = true then
   begin
+    e_LogWritefln('g_Console_ReadConfig (2) "%s"', [filename]);
     AssignFile(f, filename);
     Reset(f);
     while not EOF(f) do
@@ -1908,7 +1919,7 @@ procedure g_Console_WriteConfig (filename: String);
 begin
   AssignFile(f, filename);
   Rewrite(f);
-  WriteLn(f, '// generated by doom2d, do not modify');
+  WriteLn(f, '// ' + configComment);
   WriteLn(f, 'unbindall');
   for i := 0 to e_MaxInputKeys - 1 do
     if (Length(gInputBinds[i].down) > 0) or (Length(gInputBinds[i].up) > 0) then
@@ -1950,10 +1961,13 @@ begin
 end;
 
 procedure g_Console_WriteGameConfig;
+  var s: AnsiString;
 begin
-  if gParsingBinds then
-    Exit;
-  g_Console_WriteConfig(GameDir + '/dfconfig.cfg');
+  if gParsingBinds = false then
+  begin
+    s := e_GetDir(ConfigDirs);
+    g_Console_WriteConfig(e_CatPath(s, configScript))
+  end
 end;
 
 initialization

src/game/g_gui.pas
@@ -401,6 +401,7 @@ type
     procedure OnMessage(var Msg: TMessage); override;
     procedure Draw(); override;
     procedure AddItem(Item: String);
+    function ItemExists (item: String): Boolean;
     procedure SelectItem(Item: String);
     procedure Clear();
     function  GetWidth(): Integer; override;
@@ -420,22 +421,21 @@ type
 
   TGUIFileListBox = class(TGUIListBox)
   private
-    FBasePath: String;
-    FPath: String;
+    FSubPath: String;
     FFileMask: String;
     FDirs: Boolean;
+    FBaseList: SSArray; // highter index have highter priority
 
-    procedure OpenDir(path: String);
+    procedure ScanDirs;
 
   public
-    procedure OnMessage(var Msg: TMessage); override;
-    procedure SetBase(path: String);
+    procedure OnMessage (var Msg: TMessage); override;
+    procedure SetBase (dirs: SSArray; path: String = '');
     function  SelectedItem(): String;
-    procedure UpdateFileList();
+    procedure UpdateFileList;
 
     property Dirs: Boolean read FDirs write FDirs;
     property FileMask: String read FFileMask write FFileMask;
-    property Path: String read FPath;
   end;
 
   TGUIMemo = class(TGUIControl)
@@ -551,7 +551,7 @@ implementation
 
 uses
   {$INCLUDE ../nogl/noGLuses.inc}
-  g_textures, g_sound, SysUtils,
+  g_textures, g_sound, SysUtils, e_res,
   g_game, Math, StrUtils, g_player, g_options,
   g_map, g_weapons, xdynrec, wadreader;
 
@@ -2983,7 +2983,15 @@ begin
   if FSort then g_Basic.Sort(FItems);
 end;
 
-procedure TGUIListBox.Clear();
+function TGUIListBox.ItemExists (item: String): Boolean;
+  var i: Integer;
+begin
+  i := 0;
+  while (i <= High(FItems)) and (FItems[i] <> item) do Inc(i);
+  result := i <= High(FItems)
+end;
+
+procedure TGUIListBox.Clear;
 begin
   FItems := nil;
 
@@ -3173,7 +3181,7 @@ end;
 
 procedure TGUIFileListBox.OnMessage(var Msg: TMessage);
 var
-  a, b: Integer;
+  a, b: Integer; s: AnsiString;
 begin
   if not FEnabled then
     Exit;
@@ -3256,7 +3264,18 @@ begin
                 begin
                   if FItems[FIndex][1] = #29 then // Папка
                   begin
-                    OpenDir(FPath+Copy(FItems[FIndex], 2, 255));
+                    if FItems[FIndex] = #29 + '..' then
+                    begin
+                      e_LogWritefln('TGUIFileListBox: Upper dir "%s" -> "%s"', [FSubPath, e_UpperDir(FSubPath)]);
+                      FSubPath := e_UpperDir(FSubPath)
+                    end
+                    else
+                    begin
+                      s := Copy(AnsiString(FItems[FIndex]), 2);
+                      e_LogWritefln('TGUIFileListBox: Enter dir "%s" -> "%s"', [FSubPath, e_CatPath(FSubPath, s)]);
+                      FSubPath := e_CatPath(FSubPath, s);
+                    end;
+                    ScanDirs;
                     FIndex := 0;
                     Exit;
                   end;
@@ -3289,70 +3308,78 @@ begin
     end;
 end;
 
-procedure TGUIFileListBox.OpenDir(path: String);
-var
-  SR: TSearchRec;
-  i: Integer;
-  sm, sc: string;
+procedure TGUIFileListBox.ScanDirs;
+  var i, j: Integer; path: AnsiString; SR: TSearchRec; sm, sc: String;
 begin
-  Clear();
+  Clear;
 
-  path := IncludeTrailingPathDelimiter(path);
-  path := ExpandFileName(path);
-
-  // Каталоги:
-  if FDirs then
+  i := High(FBaseList);
+  while i >= 0 do
   begin
-    if FindFirst(path+'*', faDirectory, SR) = 0 then
-    repeat
-      if not LongBool(SR.Attr and faDirectory) then
-        Continue;
-      if (SR.Name = '.') or
-         ((SR.Name = '..') and (path = ExpandFileName(FBasePath))) then
-        Continue;
-
-      AddItem(#1 + SR.Name);
-    until FindNext(SR) <> 0;
-
-    FindClose(SR);
+    path := e_CatPath(FBaseList[i], FSubPath);
+    if FDirs then
+    begin
+      if FindFirst(path + '/' + '*', faDirectory, SR) = 0 then
+      begin
+        repeat
+          if LongBool(SR.Attr and faDirectory) then
+            if (SR.Name <> '.') and ((FSubPath <> '') or (SR.Name <> '..')) then
+              if Self.ItemExists(#1 + SR.Name) = false then
+                Self.AddItem(#1 + SR.Name)
+        until FindNext(SR) <> 0
+      end;
+      FindClose(SR)
+    end;
+    Dec(i)
   end;
 
-  // Файлы:
-  sm := FFileMask;
-  while sm <> '' do
+  i := High(FBaseList);
+  while i >= 0 do
   begin
-    i := Pos('|', sm);
-    if i = 0 then i := length(sm)+1;
-    sc := Copy(sm, 1, i-1);
-    Delete(sm, 1, i);
-    if FindFirst(path+sc, faAnyFile, SR) = 0 then repeat AddItem(SR.Name); until FindNext(SR) <> 0;
-    FindClose(SR);
+    path := e_CatPath(FBaseList[i], FSubPath);
+    sm := FFileMask;
+    while sm <> '' do
+    begin
+      j := Pos('|', sm);
+      if j = 0 then
+        j := length(sm) + 1;
+      sc := Copy(sm, 1, j - 1);
+      Delete(sm, 1, j);
+      if FindFirst(path + '/' + sc, faAnyFile, SR) = 0 then
+      begin
+        repeat
+          if Self.ItemExists(SR.Name) = false then
+            AddItem(SR.Name)
+        until FindNext(SR) <> 0
+      end;
+      FindClose(SR)
+    end;
+    Dec(i)
   end;
 
   for i := 0 to High(FItems) do
     if FItems[i][1] = #1 then
       FItems[i][1] := #29;
-
-  FPath := path;
 end;
 
-procedure TGUIFileListBox.SetBase(path: String);
+procedure TGUIFileListBox.SetBase (dirs: SSArray; path: String = '');
 begin
-  FBasePath := path;
-  OpenDir(FBasePath);
+  FBaseList := dirs;
+  FSubPath := path;
+  ScanDirs
 end;
 
-function TGUIFileListBox.SelectedItem(): String;
+function TGUIFileListBox.SelectedItem (): String;
+  var s: AnsiString;
 begin
-  Result := '';
-
-  if (FIndex = -1) or (FItems = nil) or
-     (FIndex > High(FItems)) or
-     (FItems[FIndex][1] = '/') or
-     (FItems[FIndex][1] = '\') then
-    Exit;
-
-  Result := FPath + FItems[FIndex];
+  result := '';
+  if (FIndex >= 0) and (FIndex <= High(FItems)) and (FItems[FIndex][1] <> '/') and (FItems[FIndex][1] <> '\') then
+  begin
+    s := e_CatPath(FSubPath, FItems[FIndex]);
+    if e_FindResource(FBaseList, s) = true then
+      result := ExpandFileName(s)
+  end;
+  e_LogWritefln('TGUIFileListBox.SelectedItem -> "%s"', [result]);
 end;
 
 procedure TGUIFileListBox.UpdateFileList();
@@ -3367,7 +3394,8 @@ begin
   else
     fn := FItems[FIndex];
 
-  OpenDir(FPath);
+//  OpenDir(FPath);
+  ScanDirs;
 
   if fn <> '' then
     SelectItem(fn);

src/game/g_holmes.pas
@@ -48,7 +48,7 @@ implementation
 
 uses
   {$INCLUDE ../nogl/noGLuses.inc}
-  {rttiobj,} typinfo, e_texture,
+  {rttiobj,} typinfo, e_texture, e_res,
   SysUtils, Classes, SDL2,
   MAPDEF, g_main, g_options,
   utils, hashtable, xparser;
@@ -1740,7 +1740,7 @@ begin
 
     // load bindings from file
     try
-      st := openDiskFileRO(GameDir+'holmes.rc');
+      st := e_OpenResourceRO(ConfigDirs, 'holmes.rc');
       pr := TFileTextParser.Create(st);
       conwriteln('parsing "holmes.rc"...');
       while (pr.tokType <> pr.TTEOF) do

commit ce24e997b94d04fa00b002ab2786cddc060868a9

Fix 'bot_addlist' console command not working Regression was introduced in 414f2873efa0cce84499f64774db7000e6268971.

src/game/g_player.pas
@@ -1217,90 +1217,91 @@ var
   path: AnsiString;
 begin
   BotNames := nil;
+  BotList := nil;
 
   path := BOTNAMES_FILENAME;
-  if e_FindResource(DataDirs, path) = false then
-    Exit;
-
-// Читаем возможные имена ботов из файла:
-  AssignFile(F, path);
-  Reset(F);
-
-  while not EOF(F) do
+  if e_FindResource(DataDirs, path) then
   begin
-    ReadLn(F, s);
+    // Читаем возможные имена ботов из файла:
+    AssignFile(F, path);
+    Reset(F);
 
-    s := Trim(s);
-    if s = '' then
-      Continue;
-
-    SetLength(BotNames, Length(BotNames)+1);
-    BotNames[High(BotNames)] := s;
-  end;
-
-  CloseFile(F);
-
-// Перемешиваем их:
-  g_Bot_MixNames();
-
-// Читаем файл с параметрами ботов:
-  config := TConfig.CreateFile(path);
-  BotList := nil;
-  a := 0;
-
-  while config.SectionExists(IntToStr(a)) do
-  begin
-    SetLength(BotList, Length(BotList)+1);
-
-    with BotList[High(BotList)] do
+    while not EOF(F) do
     begin
-    // Имя бота:
-      name := config.ReadStr(IntToStr(a), 'name', '');
-    // Модель:
-      model := config.ReadStr(IntToStr(a), 'model', '');
-    // Команда:
-      if config.ReadStr(IntToStr(a), 'team', 'red') = 'red' then
-        team := TEAM_RED
-      else
-        team := TEAM_BLUE;
-    // Цвет модели:
-      sa := parse(config.ReadStr(IntToStr(a), 'color', ''));
-      color.R := StrToIntDef(sa[0], 0);
-      color.G := StrToIntDef(sa[1], 0);
-      color.B := StrToIntDef(sa[2], 0);
-    // Вероятность стрельбы под углом:
-      diag_fire := config.ReadInt(IntToStr(a), 'diag_fire', 0);
-    // Вероятность ответного огня по невидимому сопернику:
-      invis_fire := config.ReadInt(IntToStr(a), 'invis_fire', 0);
-    // Точность стрельбы под углом:
-      diag_precision := config.ReadInt(IntToStr(a), 'diag_precision', 0);
-    // Точность стрельбы в полете:
-      fly_precision := config.ReadInt(IntToStr(a), 'fly_precision', 0);
-    // Точность уклонения от снарядов:
-      cover := config.ReadInt(IntToStr(a), 'cover', 0);
-    // Вероятность прыжка при приближении соперника:
-      close_jump := config.ReadInt(IntToStr(a), 'close_jump', 0);
-    // Приоритеты оружия для дальнего боя:
-      sa := parse(config.ReadStr(IntToStr(a), 'w_prior1', ''));
-      if Length(sa) = 10 then
-        for b := 0 to 9 do
-          w_prior1[b] := EnsureRange(StrToInt(sa[b]), 0, 9);
-    // Приоритеты оружия для ближнего боя:
-      sa := parse(config.ReadStr(IntToStr(a), 'w_prior2', ''));
-      if Length(sa) = 10 then
-        for b := 0 to 9 do
-          w_prior2[b] := EnsureRange(StrToInt(sa[b]), 0, 9);
+      ReadLn(F, s);
 
-      {sa := parse(config.ReadStr(IntToStr(a), 'w_prior3', ''));
-      if Length(sa) = 10 then
-        for b := 0 to 9 do
-          w_prior3[b] := EnsureRange(StrToInt(sa[b]), 0, 9);}
+      s := Trim(s);
+      if s = '' then
+        Continue;
+
+      SetLength(BotNames, Length(BotNames)+1);
+      BotNames[High(BotNames)] := s;
     end;
 
-    a := a + 1;
+    CloseFile(F);
+
+    // Перемешиваем их:
+    g_Bot_MixNames();
   end;
 
-  config.Free();
+  path := BOTLIST_FILENAME;
+  if e_FindResource(DataDirs, path) then
+  begin
+    // Читаем файл с параметрами ботов:
+    config := TConfig.CreateFile(path);
+    a := 0;
+
+    while config.SectionExists(IntToStr(a)) do
+    begin
+      SetLength(BotList, Length(BotList)+1);
+
+      with BotList[High(BotList)] do
+      begin
+        name := config.ReadStr(IntToStr(a), 'name', '');  // Имя бота
+        model := config.ReadStr(IntToStr(a), 'model', '');  // Модель
+
+        // Команда
+        if config.ReadStr(IntToStr(a), 'team', 'red') = 'red'
+          then team := TEAM_RED
+          else team := TEAM_BLUE;
+
+        // Цвет модели
+        sa := parse(config.ReadStr(IntToStr(a), 'color', ''));
+        color.R := StrToIntDef(sa[0], 0);
+        color.G := StrToIntDef(sa[1], 0);
+        color.B := StrToIntDef(sa[2], 0);
+
+        diag_fire := config.ReadInt(IntToStr(a), 'diag_fire', 0);  // Вероятность стрельбы под углом
+        invis_fire := config.ReadInt(IntToStr(a), 'invis_fire', 0);  // Вероятность ответного огня по невидимому сопернику
+        diag_precision := config.ReadInt(IntToStr(a), 'diag_precision', 0);  // Точность стрельбы под углом
+        fly_precision := config.ReadInt(IntToStr(a), 'fly_precision', 0);  // Точность стрельбы в полете
+        cover := config.ReadInt(IntToStr(a), 'cover', 0);  // Точность уклонения от снарядов
+        close_jump := config.ReadInt(IntToStr(a), 'close_jump', 0);  // Вероятность прыжка при приближении соперника
+
+        // Приоритеты оружия для дальнего боя
+        sa := parse(config.ReadStr(IntToStr(a), 'w_prior1', ''));
+        if Length(sa) = 10 then
+          for b := 0 to 9 do
+            w_prior1[b] := EnsureRange(StrToInt(sa[b]), 0, 9);
+
+        // Приоритеты оружия для ближнего боя
+        sa := parse(config.ReadStr(IntToStr(a), 'w_prior2', ''));
+        if Length(sa) = 10 then
+          for b := 0 to 9 do
+            w_prior2[b] := EnsureRange(StrToInt(sa[b]), 0, 9);
+
+        {sa := parse(config.ReadStr(IntToStr(a), 'w_prior3', ''));
+        if Length(sa) = 10 then
+          for b := 0 to 9 do
+            w_prior3[b] := EnsureRange(StrToInt(sa[b]), 0, 9);}
+      end;
+
+      a += 1;
+    end;
+
+    config.Free();
+  end;
+
   SetLength(SavedStates, 0);
 end;
 