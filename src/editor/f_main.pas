Doom2D/Doom2D-Forever

commit 29a903694117852740ab3eb9814fb46eb103c3b3

Improve and refactor processing of layers and contours Fixes https://doom2d.org/forum/viewtopic.php?f=60&t=3138

lang/editor.ru_RU.lng
@@ -769,24 +769,26 @@ g_language.MsgMenuHelp  = "Справка"
 g_language.MsgMenuHelpAboutWin$ = "About Editor"
 g_language.MsgMenuHelpAboutWin  = "О программе Editor"
 
-g_language.MsgMenuLayerBack$ = "1. Background"
-g_language.MsgMenuLayerBack  = "1. Фон"
-g_language.MsgMenuLayerWall$ = "2. Walls"
-g_language.MsgMenuLayerWall  = "2. Стены"
-g_language.MsgMenuLayerFore$ = "3. Foreground"
-g_language.MsgMenuLayerFore  = "3. Передний план"
-g_language.MsgMenuLayerStair$ = "4. Steps"
-g_language.MsgMenuLayerStair  = "4. Ступени"
-g_language.MsgMenuLayerWater$ = "5. Liquids"
-g_language.MsgMenuLayerWater  = "5. Жидкости"
-g_language.MsgMenuLayerItem$ = "6. Items"
-g_language.MsgMenuLayerItem  = "6. Предметы"
-g_language.MsgMenuLayerMonster$ = "7. Monsters"
-g_language.MsgMenuLayerMonster  = "7. Монстры"
-g_language.MsgMenuLayerArea$ = "8. Areas"
-g_language.MsgMenuLayerArea  = "8. Области"
-g_language.MsgMenuLayerTrigger$ = "9. Triggers"
-g_language.MsgMenuLayerTrigger  = "9. Триггеры"
+g_language.MsgMenuLayerBack$ = "Background"
+g_language.MsgMenuLayerBack  = "Фон"
+g_language.MsgMenuLayerWall$ = "Walls && Doors"
+g_language.MsgMenuLayerWall  = "Стены и двери"
+g_language.MsgMenuLayerFore$ = "Foreground"
+g_language.MsgMenuLayerFore  = "Передний план"
+g_language.MsgMenuLayerZones$ = "Streams && Zones"
+g_language.MsgMenuLayerZones  = "Лифты, потоки и зоны"
+g_language.MsgMenuLayerStair$ = "Steps"
+g_language.MsgMenuLayerStair  = "Ступени"
+g_language.MsgMenuLayerWater$ = "Liquids"
+g_language.MsgMenuLayerWater  = "Жидкости"
+g_language.MsgMenuLayerMonster$ = "Monsters"
+g_language.MsgMenuLayerMonster  = "Монстры"
+g_language.MsgMenuLayerArea$ = "Areas"
+g_language.MsgMenuLayerArea  = "Области"
+g_language.MsgMenuLayerItem$ = "Items"
+g_language.MsgMenuLayerItem  = "Предметы"
+g_language.MsgMenuLayerTrigger$ = "Triggers"
+g_language.MsgMenuLayerTrigger  = "Триггеры"
 
 g_language.MsgMenuTbNew$ = "New Map"
 g_language.MsgMenuTbNew  = "Новая карта"

src/editor/f_main.lfm
@@ -696,11 +696,10 @@ object MainForm: TMainForm
     end
     object tbShow: TToolButton
       Left = 172
-      Hint = 'Отрисовка панелей/объектов'
       Top = 2
-      Caption = 'tbShow'
-      DropdownMenu = pmShow
-      ImageIndex = 4
+      Caption = 'Show Layers'
+      ImageIndex = 25
+      MenuItem = miLayers
       OnClick = tbShowClick
       Style = tbsDropDown
     end
@@ -1108,51 +1107,84 @@ object MainForm: TMainForm
       object miLayers: TMenuItem
         Caption = 'Show Layers'
         ImageIndex = 25
-        object miLayer1: TMenuItem
-          Caption = 'Фон'
+        object miLayerBackground: TMenuItem
+          AutoCheck = True
+          Caption = 'Background'
           Checked = True
-          SubMenuImages = ImageList
-          OnClick = miLayer1Click
+          ShortCut = 49
+          OnClick = miLayerClick
         end
-        object miLayer2: TMenuItem
-          Caption = 'Стены'
+        object miLayerWallsDoors: TMenuItem
+          AutoCheck = True
+          Caption = 'Walls && Doors'
           Checked = True
-          OnClick = miLayer2Click
+          ShortCut = 50
+          OnClick = miLayerClick
         end
-        object miLayer3: TMenuItem
-          Caption = 'Передний план'
+        object miLayerForeground: TMenuItem
+          AutoCheck = True
+          Caption = 'Foreground'
           Checked = True
-          OnClick = miLayer3Click
+          ShortCut = 51
+          OnClick = miLayerClick
         end
-        object miLayer4: TMenuItem
-          Caption = 'Лестницы'
-          Checked = True
-          OnClick = miLayer4Click
+        object miLayersSep1: TMenuItem
+          Caption = '-'
         end
-        object miLayer5: TMenuItem
-          Caption = 'Жидкости'
+        object miLayerStreamsZones: TMenuItem
+          AutoCheck = True
+          Caption = 'Streams && Zones'
           Checked = True
-          OnClick = miLayer5Click
+          ShortCut = 52
+          OnClick = miLayerClick
         end
-        object miLayer6: TMenuItem
-          Caption = 'Предметы'
+        object miLayerSteps: TMenuItem
+          AutoCheck = True
+          Caption = 'Steps'
           Checked = True
-          OnClick = miLayer6Click
+          ShortCut = 53
+          OnClick = miLayerClick
         end
-        object miLayer7: TMenuItem
-          Caption = 'Монстры'
+        object miLayerLiquids: TMenuItem
+          AutoCheck = True
+          Caption = 'Liquids'
           Checked = True
-          OnClick = miLayer7Click
+          ShortCut = 54
+          OnClick = miLayerClick
         end
-        object miLayer8: TMenuItem
-          Caption = 'Области'
-          Checked = True
-          OnClick = miLayer8Click
+        object miLayersSep2: TMenuItem
+          Caption = '-'
         end
-        object miLayer9: TMenuItem
-          Caption = 'Триггеры'
+        object miLayerMonsters: TMenuItem
+          AutoCheck = True
+          Caption = 'Monsters'
           Checked = True
-          OnClick = miLayer9Click
+          ShortCut = 55
+          OnClick = miLayerClick
+        end
+        object miLayerAreas: TMenuItem
+          AutoCheck = True
+          Caption = 'Areas'
+          Checked = True
+          ShortCut = 56
+          OnClick = miLayerClick
+        end
+        object miLayerItems: TMenuItem
+          AutoCheck = True
+          Caption = 'Items'
+          Checked = True
+          ShortCut = 57
+          OnClick = miLayerClick
+        end
+        object miLayersSep3: TMenuItem
+          Caption = '-'
+        end
+        object miLayerTriggers: TMenuItem
+          AutoCheck = True
+          Caption = 'Triggers'
+          Checked = True
+          ShortCut = 48
+          OnClick = miLayerClick
         end
       end
       object miViewLine1: TMenuItem
@@ -1234,56 +1266,6 @@ object MainForm: TMainForm
         ShortCut = 49217
         OnClick = selectall1Click
       end
-    end
-  end
-  object pmShow: TPopupMenu
-    Images = ImageList
-    Left = 128
-    Top = 64
-    object miLayerP1: TMenuItem
-      Caption = 'Фон'
-      Checked = True
-      OnClick = miLayer1Click
-    end
-    object miLayerP2: TMenuItem
-      Caption = 'Стены'
-      Checked = True
-      OnClick = miLayer2Click
-    end
-    object miLayerP3: TMenuItem
-      Caption = 'Передний план'
-      Checked = True
-      OnClick = miLayer3Click
-    end
-    object miLayerP4: TMenuItem
-      Caption = 'Лестницы'
-      Checked = True
-      OnClick = miLayer4Click
-    end
-    object miLayerP5: TMenuItem
-      Caption = 'Жидкости'
-      Checked = True
-      OnClick = miLayer5Click
-    end
-    object miLayerP6: TMenuItem
-      Caption = 'Предметы'
-      Checked = True
-      OnClick = miLayer6Click
-    end
-    object miLayerP7: TMenuItem
-      Caption = 'Монстры'
-      Checked = True
-      OnClick = miLayer7Click
-    end
-    object miLayerP8: TMenuItem
-      Caption = 'Области'
-      Checked = True
-      OnClick = miLayer8Click
-    end
-    object miLayerP9: TMenuItem
-      Caption = 'Триггеры'
-      Checked = True
-      OnClick = miLayer9Click
     end
   end
   object ilToolbar: TImageList

commit 80fbabd697ce89c1bdcc8591612467b7432d006d

Remap layers and contours onto functional keys and make them distinct This addresses issues introduced by 29a90369.

lang/editor.ru_RU.lng
@@ -771,24 +771,28 @@ g_language.MsgMenuHelpAboutWin  = "О программе Editor"
 
 g_language.MsgMenuLayerBack$ = "Background"
 g_language.MsgMenuLayerBack  = "Фон"
-g_language.MsgMenuLayerWall$ = "Walls && Doors"
-g_language.MsgMenuLayerWall  = "Стены и двери"
 g_language.MsgMenuLayerFore$ = "Foreground"
 g_language.MsgMenuLayerFore  = "Передний план"
-g_language.MsgMenuLayerZones$ = "Streams && Zones"
-g_language.MsgMenuLayerZones  = "Лифты, потоки и зоны"
-g_language.MsgMenuLayerStair$ = "Steps"
-g_language.MsgMenuLayerStair  = "Ступени"
-g_language.MsgMenuLayerWater$ = "Liquids"
-g_language.MsgMenuLayerWater  = "Жидкости"
-g_language.MsgMenuLayerMonster$ = "Monsters"
-g_language.MsgMenuLayerMonster  = "Монстры"
-g_language.MsgMenuLayerArea$ = "Areas"
-g_language.MsgMenuLayerArea  = "Области"
-g_language.MsgMenuLayerItem$ = "Items"
-g_language.MsgMenuLayerItem  = "Предметы"
-g_language.MsgMenuLayerTrigger$ = "Triggers"
-g_language.MsgMenuLayerTrigger  = "Триггеры"
+g_language.MsgMenuLayerWalls$ = "Walls"
+g_language.MsgMenuLayerWalls  = "Стены"
+g_language.MsgMenuLayerDoors$ = "Doors"
+g_language.MsgMenuLayerDoors  = "Двери"
+g_language.MsgMenuLayerStairs$ = "Steps"
+g_language.MsgMenuLayerStairs  = "Ступени"
+g_language.MsgMenuLayerLiquids$ = "Liquids"
+g_language.MsgMenuLayerLiquids  = "Жидкости"
+g_language.MsgMenuLayerStreams$ = "Streams"
+g_language.MsgMenuLayerStreams  = "Лифты и потоки"
+g_language.MsgMenuLayerZones$ = "Zoning"
+g_language.MsgMenuLayerZones  = "Зонирование"
+g_language.MsgMenuLayerMonsters$ = "Monsters"
+g_language.MsgMenuLayerMonsters  = "Монстры"
+g_language.MsgMenuLayerAreas$ = "Areas"
+g_language.MsgMenuLayerAreas  = "Области"
+g_language.MsgMenuLayerItems$ = "Items"
+g_language.MsgMenuLayerItems  = "Предметы"
+g_language.MsgMenuLayerTriggers$ = "Triggers"
+g_language.MsgMenuLayerTriggers  = "Триггеры"
 
 g_language.MsgMenuTbNew$ = "New Map"
 g_language.MsgMenuTbNew  = "Новая карта"

src/editor/f_main.lfm
@@ -954,7 +954,6 @@ object MainForm: TMainForm
       object miReopenMap: TMenuItem
         Caption = 'Revert to Saved'
         ImageIndex = 1
-        ShortCut = 116
         OnClick = miReopenMapClick
       end
       object miLine1: TMenuItem
@@ -994,7 +993,6 @@ object MainForm: TMainForm
         Caption = 'Exit'
         Enabled = False
         ImageIndex = 10
-        ShortCut = 121
         Visible = False
         OnClick = aExitExecute
       end
@@ -1098,46 +1096,58 @@ object MainForm: TMainForm
           AutoCheck = True
           Caption = 'Background'
           Checked = True
-          ShortCut = 49
-          OnClick = miLayerClick
-        end
-        object miLayerWallsDoors: TMenuItem
-          AutoCheck = True
-          Caption = 'Walls && Doors'
-          Checked = True
-          ShortCut = 50
+          ShortCut = 112
           OnClick = miLayerClick
         end
         object miLayerForeground: TMenuItem
           AutoCheck = True
           Caption = 'Foreground'
           Checked = True
-          ShortCut = 51
+          ShortCut = 113
           OnClick = miLayerClick
+        end
+        object miLayerWalls: TMenuItem
+          AutoCheck = True
+          Caption = 'Walls'
+          Checked = True
+          ShortCut = 114
+          OnClick = miLayerClick
+        end
+        object miLayerDoors: TMenuItem
+          AutoCheck = True
+          Caption = 'Doors'
+          Checked = True
+          ShortCut = 115
         end
         object miLayersSep1: TMenuItem
           Caption = '-'
-        end
-        object miLayerStreamsZones: TMenuItem
-          AutoCheck = True
-          Caption = 'Streams && Zones'
-          Checked = True
-          ShortCut = 52
-          OnClick = miLayerClick
         end
         object miLayerSteps: TMenuItem
           AutoCheck = True
           Caption = 'Steps'
           Checked = True
-          ShortCut = 53
+          ShortCut = 116
           OnClick = miLayerClick
         end
         object miLayerLiquids: TMenuItem
           AutoCheck = True
           Caption = 'Liquids'
           Checked = True
-          ShortCut = 54
+          ShortCut = 117
           OnClick = miLayerClick
+        end
+        object miLayerStreams: TMenuItem
+          AutoCheck = True
+          Caption = 'Streams'
+          Checked = True
+          ShortCut = 118
+          OnClick = miLayerClick
+        end
+        object miLayerZones: TMenuItem
+          AutoCheck = True
+          Caption = 'Zoning'
+          Checked = True
+          ShortCut = 119
         end
         object miLayersSep2: TMenuItem
           Caption = '-'
@@ -1146,31 +1156,28 @@ object MainForm: TMainForm
           AutoCheck = True
           Caption = 'Monsters'
           Checked = True
-          ShortCut = 55
+          ShortCut = 120
           OnClick = miLayerClick
         end
         object miLayerAreas: TMenuItem
           AutoCheck = True
           Caption = 'Areas'
           Checked = True
-          ShortCut = 56
+          ShortCut = 121
           OnClick = miLayerClick
         end
         object miLayerItems: TMenuItem
           AutoCheck = True
           Caption = 'Items'
           Checked = True
-          ShortCut = 57
+          ShortCut = 122
           OnClick = miLayerClick
-        end
-        object miLayersSep3: TMenuItem
-          Caption = '-'
         end
         object miLayerTriggers: TMenuItem
           AutoCheck = True
           Caption = 'Triggers'
           Checked = True
-          ShortCut = 48
+          ShortCut = 123
           OnClick = miLayerClick
         end
       end

src/editor/f_main.pas
@@ -74,17 +74,18 @@ type
     miMenuView: TMenuItem;
     miLayers: TMenuItem;
     miLayerBackground: TMenuItem;
-    miLayerWallsDoors: TMenuItem;
     miLayerForeground: TMenuItem;
+    miLayerWalls: TMenuItem;
+    miLayerDoors: TMenuItem;
     miLayersSep1: TMenuItem;
-    miLayerStreamsZones: TMenuItem;
     miLayerSteps: TMenuItem;
     miLayerLiquids: TMenuItem;
+    miLayerStreams: TMenuItem;
+    miLayerZones: TMenuItem;
     miLayersSep2: TMenuItem;
-    miLayerItems: TMenuItem;
     miLayerMonsters: TMenuItem;
     miLayerAreas: TMenuItem;
-    miLayersSep3: TMenuItem;
+    miLayerItems: TMenuItem;
     miLayerTriggers: TMenuItem;
     miViewLine1: TMenuItem;
     miMiniMap: TMenuItem;
@@ -4432,7 +4433,7 @@ begin
     if ssCtrl in Shift then
     begin
       case Key of
-        VK_0..VK_9: begin
+        VK_F1..VK_F12: begin
           ContourItem := MainMenu.FindItem(PtrInt(Key), fkShortCut);  // must always succeed!
           ContourItem.Tag := not ContourItem.Tag;
         end;

src/editor/g_language.pas
@@ -389,15 +389,17 @@ Interface
     MsgMenuHelpAboutWin = 'About Editor';
 
     MsgMenuLayerBack = 'Background';
-    MsgMenuLayerWall = 'Walls && Doors';
     MsgMenuLayerFore = 'Foreground';
-    MsgMenuLayerZones = 'Streams && Zones';
-    MsgMenuLayerStair = 'Steps';
-    MsgMenuLayerWater = 'Liquids';
-    MsgMenuLayerMonster = 'Monsters';
-    MsgMenuLayerArea = 'Areas';
-    MsgMenuLayerItem = 'Items';
-    MsgMenuLayerTrigger = 'Triggers';
+    MsgMenuLayerWalls = 'Walls';
+    MsgMenuLayerDoors = 'Doors';
+    MsgMenuLayerStairs = 'Steps';
+    MsgMenuLayerLiquids = 'Liquids';
+    MsgMenuLayerStreams = 'Streams';
+    MsgMenuLayerZones = 'Zoning';
+    MsgMenuLayerMonsters = 'Monsters';
+    MsgMenuLayerAreas = 'Areas';
+    MsgMenuLayerItems = 'Items';
+    MsgMenuLayerTriggers = 'Triggers';
 
     MsgMenuTbNew = 'New Map';
     MsgMenuTbOpen = 'Open Map';
@@ -935,15 +937,17 @@ begin
     miMenuView.Caption := MsgMenuView;
     miLayers.Caption := MsgMenuViewLayers;
     miLayerBackground.Caption := MsgMenuLayerBack;
-    miLayerWallsDoors.Caption := MsgMenuLayerWall;
     miLayerForeground.Caption := MsgMenuLayerFore;
-    miLayerStreamsZones.Caption := MsgMenuLayerZones;
-    miLayerSteps.Caption := MsgMenuLayerStair;
-    miLayerLiquids.Caption := MsgMenuLayerWater;
-    miLayerMonsters.Caption := MsgMenuLayerMonster;
-    miLayerAreas.Caption := MsgMenuLayerArea;
-    miLayerItems.Caption := MsgMenuLayerItem;
-    miLayerTriggers.Caption := MsgMenuLayerTrigger;
+    miLayerWalls.Caption := MsgMenuLayerWalls;
+    miLayerDoors.Caption := MsgMenuLayerDoors;
+    miLayerSteps.Caption := MsgMenuLayerStairs;
+    miLayerLiquids.Caption := MsgMenuLayerLiquids;
+    miLayerStreams.Caption := MsgMenuLayerStreams;
+    miLayerZones.Caption := MsgMenuLayerZones;
+    miLayerMonsters.Caption := MsgMenuLayerMonsters;
+    miLayerAreas.Caption := MsgMenuLayerAreas;
+    miLayerItems.Caption := MsgMenuLayerItems;
+    miLayerTriggers.Caption := MsgMenuLayerTriggers;
     miMiniMap.Caption := MsgMenuViewMinimap;
     miShowEdges.Caption := MsgMenuViewBounds;
     miMapPreview.Caption := MsgMenuViewPreview;

src/editor/g_map.pas
@@ -581,18 +581,19 @@ begin
   case PanelType of
     PANEL_BACK: Result := MainForm.miLayerBackground.Checked;
     PANEL_FORE: Result := MainForm.miLayerForeground.Checked;
+    PANEL_WALL: Result := MainForm.miLayerWalls.Checked;
     PANEL_STEP: Result := MainForm.miLayerSteps.Checked;
+    PANEL_BLOCKMON: Result := MainForm.miLayerZones.Checked;
 
-    PANEL_WALL, PANEL_OPENDOOR, PANEL_CLOSEDOOR:
-      Result := MainForm.miLayerWallsDoors.Checked;
+    PANEL_OPENDOOR, PANEL_CLOSEDOOR:
+      Result := MainForm.miLayerDoors.Checked;
 
     PANEL_WATER, PANEL_ACID1, PANEL_ACID2:
       Result := MainForm.miLayerLiquids.Checked;
 
     PANEL_LIFTUP, PANEL_LIFTDOWN,
-    PANEL_LIFTLEFT, PANEL_LIFTRIGHT,
-    PANEL_BLOCKMON:
-      Result := MainForm.miLayerStreamsZones.Checked;
+    PANEL_LIFTLEFT, PANEL_LIFTRIGHT:
+      Result := MainForm.miLayerStreams.Checked;
 
     else
       Result := False;
@@ -2175,15 +2176,17 @@ begin
 // Рисуем панели (если Превью или если включен слой):
   if MainForm.miLayerBackground.Checked or (PreviewMode = 1) then
     DrawPanels(PANEL_BACK);
-  if MainForm.miLayerStreamsZones.Checked and (PreviewMode = 0) then
-    DrawPanels(PANEL_LIFTUP or PANEL_LIFTDOWN
-      or PANEL_LIFTLEFT or PANEL_LIFTRIGHT
-      or PANEL_BLOCKMON);
-  if MainForm.miLayerWallsDoors.Checked or (PreviewMode = 1) then
+  if PreviewMode = 0 then
   begin
-    DrawPanels(PANEL_OPENDOOR or PANEL_CLOSEDOOR);
-    DrawPanels(PANEL_WALL);
+    if MainForm.miLayerStreams.Checked then
+      DrawPanels(PANEL_LIFTUP or PANEL_LIFTDOWN or PANEL_LIFTLEFT or PANEL_LIFTRIGHT);
+    if MainForm.miLayerZones.Checked then
+      DrawPanels(PANEL_BLOCKMON);
   end;
+  if MainForm.miLayerDoors.Checked or (PreviewMode = 1) then
+    DrawPanels(PANEL_OPENDOOR or PANEL_CLOSEDOOR);
+  if MainForm.miLayerWalls.Checked or (PreviewMode = 1) then
+    DrawPanels(PANEL_WALL);
   if MainForm.miLayerSteps.Checked or (PreviewMode = 1) then
     DrawPanels(PANEL_STEP);
 
@@ -2312,7 +2315,7 @@ begin
         end;
 
 // Рисуем закрытые двери после монстров:
-  if (PreviewMode = 1) or ((PreviewMode = 2) and MainForm.miLayerWallsDoors.Checked) then
+  if (PreviewMode = 1) or ((PreviewMode = 2) and MainForm.miLayerDoors.Checked) then
     DrawPanels(PANEL_CLOSEDOOR);
 
 // Рисуем области:
@@ -2701,14 +2704,16 @@ begin
   mask := 0;
   if QWordBool(MainForm.miLayerBackground.Tag) then
     mask := mask or PANEL_BACK;
-  if QWordBool(MainForm.miLayerWallsDoors.Tag) then
-    mask := mask or PANEL_WALL or PANEL_OPENDOOR or PANEL_CLOSEDOOR;
+  if QWordBool(MainForm.miLayerDoors.Tag) then
+    mask := mask or PANEL_OPENDOOR or PANEL_CLOSEDOOR;
+  if QWordBool(MainForm.miLayerWalls.Tag) then
+    mask := mask or PANEL_WALL;
   if QWordBool(MainForm.miLayerForeground.Tag) then
     mask := mask or PANEL_FORE;
-  if QWordBool(MainForm.miLayerStreamsZones.Tag) then
-    mask := mask or PANEL_LIFTUP or PANEL_LIFTDOWN
-      or PANEL_LIFTLEFT or PANEL_LIFTRIGHT
-      or PANEL_BLOCKMON;
+  if QWordBool(MainForm.miLayerStreams.Tag) then
+    mask := mask or PANEL_LIFTUP or PANEL_LIFTDOWN or PANEL_LIFTLEFT or PANEL_LIFTRIGHT;
+  if QWordBool(MainForm.miLayerZones.Tag) then
+    mask := mask or PANEL_BLOCKMON;
   if QWordBool(MainForm.miLayerSteps.Tag) then
     mask := mask or PANEL_STEP;
   if QWordBool(MainForm.miLayerLiquids.Tag) then