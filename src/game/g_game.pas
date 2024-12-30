Doom2D/Doom2D-Forever

commit 0e680a0f9698f6cb533246f030ed4a063d906749

don't skip drawing disabled panels

src/game/g_panel.pas
@@ -270,7 +270,7 @@ var
   NoTextureID: DWORD;
   NW, NH: Word;
 begin
-  if Enabled and (FCurTexture >= 0) and
+  if {Enabled and} (FCurTexture >= 0) and
      (Width > 0) and (Height > 0) and (FAlpha < 255) and
      g_Collide(X, Y, Width, Height,
                sX, sY, sWidth, sHeight) then

commit 895a7ac29071d205be08647b2178babff8eeade3

Game: Fix open doors being drawn when `r_sq_draw` is False Broken by 0e680a0f9698f6cb533246f030ed4a063d906749.

src/game/g_game.pas
@@ -3591,7 +3591,7 @@ type
 var
   hasAmbient: Boolean;
   ambColor: TDFColor;
-  doAmbient: Boolean = false;
+  doAmbient: Boolean = False;
 
   procedure drawPanelType (profname: AnsiString; panType: DWord; doDraw: Boolean);
   var

src/game/g_map.pas
@@ -2662,30 +2662,26 @@ end;
 // old algo
 procedure g_Map_DrawPanels (PanelType: Word; hasAmbient: Boolean; constref ambColor: TDFColor);
 
-  procedure DrawPanels (constref panels: TPanelArray; drawDoors: Boolean=False);
+  procedure DrawGeneral (constref panels: TPanelArray; drawDoors: Boolean = False); inline;
   var
-    idx: Integer;
+    i: Integer;
   begin
-    if (panels <> nil) then
-    begin
-      // alas, no visible set
-      for idx := 0 to High(panels) do
-      begin
-        if not (drawDoors xor panels[idx].Door) then panels[idx].Draw(hasAmbient, ambColor);
-      end;
-    end;
+    // alas, no visible set
+    for i := 0 to High(panels) do
+      if panels[i].Enabled and (drawDoors = panels[i].Door) then
+        panels[i].Draw(hasAmbient, ambColor);
   end;
 
 begin
   case PanelType of
-    PANEL_WALL:       DrawPanels(gWalls);
-    PANEL_CLOSEDOOR:  DrawPanels(gWalls, True);
-    PANEL_BACK:       DrawPanels(gRenderBackgrounds);
-    PANEL_FORE:       DrawPanels(gRenderForegrounds);
-    PANEL_WATER:      DrawPanels(gWater);
-    PANEL_ACID1:      DrawPanels(gAcid1);
-    PANEL_ACID2:      DrawPanels(gAcid2);
-    PANEL_STEP:       DrawPanels(gSteps);
+    PANEL_WALL:       DrawGeneral(gWalls);
+    PANEL_CLOSEDOOR:  DrawGeneral(gWalls, True);
+    PANEL_BACK:       DrawGeneral(gRenderBackgrounds);
+    PANEL_FORE:       DrawGeneral(gRenderForegrounds);
+    PANEL_WATER:      DrawGeneral(gWater);
+    PANEL_ACID1:      DrawGeneral(gAcid1);
+    PANEL_ACID2:      DrawGeneral(gAcid2);
+    PANEL_STEP:       DrawGeneral(gSteps);
   end;
 end;
 

src/game/g_panel.pas
@@ -458,9 +458,10 @@ var
   NoTextureID: DWORD;
   NW, NH: Word;
 begin
-  if {Enabled and} (FCurTexture >= 0) and
-     (Width > 0) and (Height > 0) and (FAlpha < 255) {and
-     g_Collide(X, Y, Width, Height, sX, sY, sWidth, sHeight)} then
+  // TODO: Eliminate the remaining checks here? Like, they have to be done beforehand by the entity
+  // gathering code during preparation for rendering. See g_Map_CollectDrawPanels() for example.
+  if {Enabled and} (FCurTexture >= 0) and (Width > 0) and (Height > 0) and (FAlpha < 255) {and
+    g_Collide(X, Y, Width, Height, sX, sY, sWidth, sHeight)} then
   begin
     lerp(gLerpFactor, tx, ty, tw, th);
     if FTextureIDs[FCurTexture].Anim then