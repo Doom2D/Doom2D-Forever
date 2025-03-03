Doom2D/Doom2D-Forever

commit 4b6c50349b6b2c268faecbb7f4567cd4f5e0d835

Calculation gibs size no more depends on opengl

src/game/g_playermodel.pas
@@ -21,7 +21,8 @@ interface
 
 uses
   {$IFDEF USE_MEMPOOL}mempool,{$ENDIF}
-  MAPDEF, g_textures, g_basic, g_weapons, e_graphics, utils;
+  MAPDEF, g_textures, g_basic, g_weapons, e_graphics, utils,
+  ImagingTypes, Imaging, ImagingUtility;
 
 const
   A_STAND      = 0;
@@ -322,6 +323,65 @@ begin
   end;
 end;
 
+function g_PlayerModel_CalcGibSize (pData: Pointer; dataSize, x, y, w, h: Integer): TRectWH;
+  var i, j: Integer; done: Boolean; img: TImageData;
+
+  function IsVoid (i, j: Integer): Boolean;
+  begin
+    result := Byte((PByte(img.bits) + (y+j)*img.width*4 + (x+i)*4 + 3)^) = 0
+  end;
+
+begin
+  InitImage(img);
+  assert(LoadImageFromMemory(pData, dataSize, img));
+
+  (* trace x from right to left *)
+  done := false; i := 0;
+  while not done and (i < w) do
+  begin
+    j := 0;
+    while (j < h) and IsVoid(i, j) do inc(j);
+    done := (j < h) and (IsVoid(i, j) = false);
+    result.x := i;
+    inc(i);
+  end;
+
+  (* trace y from up to down *)
+  done := false; j := 0;
+  while not done and (j < h) do
+  begin
+    i := 0;
+    while (i < w) and IsVoid(i, j) do inc(i);
+    done := (i < w) and (IsVoid(i, j) = false);
+    result.y := j;
+    inc(j);
+  end;
+  
+  (* trace x from right to left *)
+  done := false; i := w - 1;
+  while not done and (i >= 0) do
+  begin
+    j := 0;
+    while (j < h) and IsVoid(i, j) do inc(j);
+    done := (j < h) and (IsVoid(i, j) = false);
+    result.width := i - result.x + 1;
+    dec(i);
+  end;
+
+  (* trace y from down to up *)
+  done := false; j := h - 1;
+  while not done and (j >= 0) do
+  begin
+    i := 0;
+    while (i < w) and IsVoid(i, j) do inc(i);
+    done := (i < w) and (IsVoid(i, j) = false);
+    result.height := j - result.y + 1;
+    dec(j);
+  end;
+
+  FreeImage(img);
+end;
+
 function g_PlayerModel_Load(FileName: string): Boolean;
 var
   ID: DWORD;
@@ -470,7 +530,8 @@ begin
         if e_CreateTextureMemEx(pData, lenpd, Gibs[a].ID, a*32, 0, 32, 32) and
           e_CreateTextureMemEx(pData2, lenpd2, Gibs[a].MaskID, a*32, 0, 32, 32) then
         begin
-          Gibs[a].Rect := e_GetTextureSize2(Gibs[a].ID);
+          //Gibs[a].Rect := e_GetTextureSize2(Gibs[a].ID);
+          Gibs[a].Rect := g_PlayerModel_CalcGibSize(pData, lenpd, a*32, 0, 32, 32);
           with Gibs[a].Rect do
             if Height > 3 then Height := Height-1-Random(2);
           Gibs[a].OnlyOne := config.ReadInt('Gibs', 'once', -1) = a+1;

src/engine/e_graphics.pas
@@ -95,7 +95,6 @@ function e_CreateTextureEx(FileName: string; var ID: DWORD; fX, fY, fWidth, fHei
 function e_CreateTextureMem(pData: Pointer; dataSize: LongInt; var ID: DWORD): Boolean;
 function e_CreateTextureMemEx(pData: Pointer; dataSize: LongInt; var ID: DWORD; fX, fY, fWidth, fHeight: Word): Boolean;
 procedure e_GetTextureSize(ID: DWORD; Width, Height: PWord);
-function e_GetTextureSize2(ID: DWORD): TRectWH;
 procedure e_DeleteTexture(ID: DWORD);
 procedure e_RemoveAllTextures();
 
@@ -368,106 +367,6 @@ procedure e_GetTextureSize(ID: DWORD; Width, Height: PWord);
 begin
  if Width <> nil then Width^ := e_Textures[ID].tx.Width;
  if Height <> nil then Height^ := e_Textures[ID].tx.Height;
-end;
-
-function e_GetTextureSize2(ID: DWORD): TRectWH;
-var
-  data: PChar;
-  x, y: Integer;
-  w, h: Word;
-  a: Boolean;
-  lastline: Integer;
-begin
- w := e_Textures[ID].tx.Width;
- h := e_Textures[ID].tx.Height;
-
- Result.Y := 0;
- Result.X := 0;
- Result.Width := w;
- Result.Height := h;
-
-{$IFNDEF USE_NANOGL} // FIXIT: nanoGL doesn't support glGetTexImage
- if e_NoGraphics then Exit;
-
- data := GetMemory(w*h*4);
- glEnable(GL_TEXTURE_2D);
- glBindTexture(GL_TEXTURE_2D, e_Textures[ID].tx.id);
- glGetTexImage(GL_TEXTURE_2D, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-
- for y := h-1 downto 0 do
- begin
-  lastline := y;
-  a := True;
-
-  for x := 1 to w-4 do
-  begin
-   a := Byte((data+y*w*4+x*4+3)^) <> 0;
-   if a then Break;
-  end;
-
-  if a then
-  begin
-   Result.Y := h-lastline;
-   Break;
-  end;
- end;
-
- for y := 0 to h-1 do
- begin
-  lastline := y;
-  a := True;
-
-  for x := 1 to w-4 do
-  begin
-   a := Byte((data+y*w*4+x*4+3)^) <> 0;
-   if a then Break;
-  end;
-
-  if a then
-  begin
-   Result.Height := h-lastline-Result.Y;
-   Break;
-  end;
- end;
-
- for x := 0 to w-1 do
- begin
-  lastline := x;
-  a := True;
-
-  for y := 1 to h-4 do
-  begin
-   a := Byte((data+y*w*4+x*4+3)^) <> 0;
-   if a then Break;
-  end;
-
-  if a then
-  begin
-   Result.X := lastline+1;
-   Break;
-  end;
- end;
-
- for x := w-1 downto 0 do
- begin
-  lastline := x;
-  a := True;
-
-  for y := 1 to h-4 do
-  begin
-   a := Byte((data+y*w*4+x*4+3)^) <> 0;
-   if a then Break;
-  end;
-
-  if a then
-  begin
-   Result.Width := lastline-Result.X+1;
-   Break;
-  end;
- end;
-
- FreeMemory(data);
-{$ENDIF}
 end;
 
 procedure e_ResizeWindow(Width, Height: Integer);

commit 681179b80c2abaa466bd1605754976a2903e67b8

Game: Fix segfault in `g_PlayerModel_CalcGibSize()` if skin gibs are paletted Regression was introduced in 4b6c50349b6b2c268faecbb7f4567cd4f5e0d835.

src/game/g_playermodel.pas
@@ -348,59 +348,63 @@ begin
 end;
 
 function g_PlayerModel_CalcGibSize (pData: Pointer; dataSize, x, y, w, h: Integer): TRectWH;
-  var i, j: Integer; done: Boolean; img: TImageData;
+var
+  img: TImageData;
 
-  function IsVoid (i, j: Integer): Boolean;
+  function IsVoid (h, v: Integer): Boolean; inline;
   begin
-    result := Byte((PByte(img.bits) + (y+j)*img.width*4 + (x+i)*4 + 3)^) = 0
+    Result := GetPixel32(img, x + h, y + v).A = 0;
   end;
 
+var
+  done: Boolean;
+  i, j: Integer;
 begin
   InitImage(img);
-  assert(LoadImageFromMemory(pData, dataSize, img));
+  Assert(LoadImageFromMemory(pData, dataSize, img));
 
-  (* trace x from right to left *)
-  done := false; i := 0;
+  // trace x from left to right
+  done := False; i := 0;
   while not done and (i < w) do
   begin
     j := 0;
-    while (j < h) and IsVoid(i, j) do inc(j);
-    done := (j < h) and (IsVoid(i, j) = false);
-    result.x := i;
-    inc(i);
+    while (j < h) and IsVoid(i, j) do j += 1;
+    done := (j < h) and not IsVoid(i, j);
+    Result.x := i;
+    i += 1;
   end;
 
-  (* trace y from up to down *)
-  done := false; j := 0;
+  // trace y from up to down
+  done := False; j := 0;
   while not done and (j < h) do
   begin
     i := 0;
-    while (i < w) and IsVoid(i, j) do inc(i);
-    done := (i < w) and (IsVoid(i, j) = false);
-    result.y := j;
-    inc(j);
+    while (i < w) and IsVoid(i, j) do i += 1;
+    done := (i < w) and not IsVoid(i, j);
+    Result.y := j;
+    j += 1;
   end;
   
-  (* trace x from right to left *)
-  done := false; i := w - 1;
+  // trace x from right to left
+  done := False; i := w - 1;
   while not done and (i >= 0) do
   begin
     j := 0;
-    while (j < h) and IsVoid(i, j) do inc(j);
-    done := (j < h) and (IsVoid(i, j) = false);
-    result.width := i - result.x + 1;
-    dec(i);
+    while (j < h) and IsVoid(i, j) do j += 1;
+    done := (j < h) and not IsVoid(i, j);
+    Result.width := i - Result.x + 1;
+    i -= 1;
   end;
 
-  (* trace y from down to up *)
-  done := false; j := h - 1;
+  // trace y from down to up
+  done := False; j := h - 1;
   while not done and (j >= 0) do
   begin
     i := 0;
-    while (i < w) and IsVoid(i, j) do inc(i);
-    done := (i < w) and (IsVoid(i, j) = false);
-    result.height := j - result.y + 1;
-    dec(j);
+    while (i < w) and IsVoid(i, j) do i += 1;
+    done := (i < w) and not IsVoid(i, j);
+    Result.height := j - Result.y + 1;
+    j -= 1;
   end;
 
   FreeImage(img);
@@ -573,7 +577,7 @@ begin
           //Gibs[a].Rect := e_GetTextureSize2(Gibs[a].ID);
           Gibs[a].Rect := g_PlayerModel_CalcGibSize(pData, lenpd, a*32, 0, 32, 32);
           with Gibs[a].Rect do
-            if Height > 3 then Height := Height-1-Random(2);
+            if Height > 3 then Height -= 1 + Random(2);
           Gibs[a].OnlyOne := config.ReadInt('Gibs', 'once', -1) = a+1;
         end;
 