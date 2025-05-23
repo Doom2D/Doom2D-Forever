52ed340c4ff78e16044f052a85208f29a14539f6
Dmitry D. Chernov <blackdoomer@yandex.ru>
2025-05-24 07:45:21 +1000

    Language: Replace the latest remaining occurrence of hardcoded game title Follow-up of e01e12eb1e4a150232b7572a6ed89a78476306b2.

--- src/game/g_game.pas
+++ src/game/g_game.pas
@@ -1418,7 +1418,7 @@ begin
     LoadFont('SMALLTXT', 'SMALLFONT', gMenuSmallFont);
 
     g_Game_ClearLoading();
-    g_Game_SetLoadingText(Format('DOOM 2D FOREVER %s', [GAME_VERSION]), 0, False);
+    g_Game_SetLoadingText(Format(GAME_TITLE+' %s', [GAME_VERSION]), 0, False);
     g_Game_SetLoadingText('', 0, False);
 
     g_Game_SetLoadingText(_lc[I_LOAD_CONSOLE], 0, False);
