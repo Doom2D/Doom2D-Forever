Doom2D/Doom2D-Forever

commit f60d0f670f7ca0691b558042bb2a8e41c1b7cdbe

Game: Simplify the processing code of "yes/no" menus

src/game/g_menu.pas
@@ -57,30 +57,14 @@ uses
   utils, wadreader, g_system;
 
 
-type TYNCallback = procedure (yes:Boolean);
-
-procedure YNKeyDownProc (win: TGUIWindow; Key: Byte);
+procedure YesNoButtonCB (ctl: TGUITextButton);
 begin
-  if win.UserData = nil then exit;
-  case Key of
-    IK_Y, IK_SPACE: TYNCallback(win.UserData)(true);
-    IK_N: TYNCallback(win.UserData)(false);
-  end;
+  if ctl.UserData <> nil
+    then TProcedure(ctl.UserData)()
+    else g_GUI_HideWindow();
 end;
 
-procedure YesButtonCB (ctl: TGUITextButton);
-begin
-  if ctl.UserData = nil then exit;
-  TYNCallback(ctl.UserData)(true);
-end;
-
-procedure NoButtonCB (ctl: TGUITextButton);
-begin
-  if ctl.UserData = nil then exit;
-  TYNCallback(ctl.UserData)(false);
-end;
-
-function CreateYNMenu (WinName, Text: String; MaxLen: Word; FontID: DWORD; ActionProc: TYNCallback): TGUIWindow;
+function CreateYNMenu (WinName, Text: String; MaxLen: Word; FontID: DWORD; ActionProc: TProcedure): TGUIWindow;
 var
   menu: TGUIMenu;
 begin
@@ -88,18 +72,21 @@ begin
   Result := TGUIWindow.Create(WinName);
   with Result do
   begin
-    //OnKeyDownEx := @YNKeyDownProc;
-    //UserData := @ActionProc;
     menu := TGUIMenu(Result.AddChild(TGUIMenu.Create(gMenuSmallFont, gMenuSmallFont, '')));
     with menu do
     begin
       Name := '__temp_yes_no_menu:'+WinName;
-      YesNo := true;
+      YesNo := True;
       AddText(Text, MaxLen);
-      with AddButton(nil, _lc[I_MENU_YES]) do begin ProcEx := @YesButtonCB; UserData := @ActionProc; end;
-      with AddButton(nil, _lc[I_MENU_NO]) do begin ProcEx := @NoButtonCB; UserData := @ActionProc; end;
+      with AddButton(nil, _lc[I_MENU_YES]) do
+      begin
+        ProcEx := @YesNoButtonCB;
+        UserData := @ActionProc;
+      end;
+      with AddButton(nil, _lc[I_MENU_NO]) do
+        ProcEx := @YesNoButtonCB;
     end;
-    DefControl := '__temp_yes_no_menu:'+WinName;
+    DefControl := menu.Name;
     SetActive(nil);
   end;
 end;
@@ -1282,7 +1269,7 @@ begin
 {$ENDIF}
 end;
 
-procedure ProcExitMenuKeyDown (yes: Boolean);
+procedure ProcExitMenuKeyDown ();
 var
   s: ShortString;
 {$IFDEF ENABLE_SOUND}
@@ -1290,12 +1277,6 @@ var
 {$ENDIF}
   res: Boolean;
 begin
-  if not yes then
-  begin
-    g_GUI_HideWindow();
-    exit;
-  end;
-
 {$IFDEF ENABLE_SOUND}
   g_Game_StopAllSounds(True);
   case (Random(18)) of

src/game/g_gui.pas
@@ -1573,7 +1573,7 @@ begin
     WM_KEYDOWN:
     begin
       case Msg.wParam of
-        IK_UP, IK_KPUP, VK_UP,JOY0_UP, JOY1_UP, JOY2_UP, JOY3_UP:
+        IK_UP, IK_KPUP, VK_UP, JOY0_UP, JOY1_UP, JOY2_UP, JOY3_UP:
         begin
           c := 0;
           repeat

commit 81d1abe9cb3656d6192b4e60952084b1f0c48b88

Game: Fix "End game" menu misbehaving on Android like "No" is always selected This complements f60d0f670f7ca0691b558042bb2a8e41c1b7cdbe, which introduced the regression.

src/game/g_menu.pas
@@ -1631,19 +1631,16 @@ begin
   TGUIModelView(g_ActiveWindow.GetControl('mv'+s+'ModelTeam')).Model.InvertDirection();
 end;
 
-procedure ProcDefaultMenuKeyDown (yes: Boolean);
+procedure ProcDefaultMenuKeyDown ();
 begin
-  if yes then
-  begin
-    g_Options_SetDefault();
-    ReadOptions();
-  end;
+  g_Options_SetDefault();
+  ReadOptions();
   g_GUI_HideWindow();
 end;
 
-procedure ProcSavedMenuKeyDown (yes: Boolean);
+procedure ProcSavedMenuKeyDown ();
 begin
-  if yes then ReadOptions();
+  ReadOptions();
   g_GUI_HideWindow();
 end;
 
@@ -1809,14 +1806,9 @@ begin
   g_Game_Pause(False);
 end;
 
-procedure ProcRestartMenuKeyDown (yes: Boolean);
+procedure ProcEndMenuKeyDown ();
 begin
-  if yes then g_Game_Restart() else g_GUI_HideWindow;
-end;
-
-procedure ProcEndMenuKeyDown (yes: Boolean);
-begin
-  if yes then gExit := EXIT_SIMPLE else g_GUI_HideWindow;
+  gExit := EXIT_SIMPLE;
 end;
 
 procedure ProcSetRussianLanguage;
@@ -3739,7 +3731,7 @@ begin
   g_GUI_AddWindow(Menu);
 
   Menu := CreateYNMenu('RestartGameMenu', _lc[I_MENU_RESTART_GAME_PROMPT], Round(gScreenWidth*0.6),
-                       gMenuSmallFont, @ProcRestartMenuKeyDown);
+                       gMenuSmallFont, @g_Game_Restart);
   g_GUI_AddWindow(Menu);
 
   Menu := g_GUI_AddWindow(TGUIWindow.Create('GameCustomMenu'));