Doom2D/Doom2D-Forever

commit dce13ae29ce979b4343cfdd2e1afdb63c34781a4

Disable `{$MODESWITCH CLASSICPROCVARS+}`, get rid of Delphi-style function pointers That weird @FObj check in g_player.pas, now removed, was introduced by this commit: https://github.com/Doom2D/DF/commit/57b0cc9af78d7f42c40fc9b1412fd6d589b18100

src/shared/utils.pas
@@ -156,6 +156,9 @@ function utf8Valid (const s: AnsiString): Boolean;
 
 function utf8to1251 (s: AnsiString): AnsiString;
 
+// complies to TComparisonFunc<ShortString> from Generics.Default
+function SSArraySortFunc(constref Left, Right: ShortString): Integer;
+
 // findFileCI takes case-insensitive path, traverses it, and rewrites it to
 // a case-sensetive one (using real on-disk names). return value means 'success'.
 // if some dir or file wasn't found, pathname is undefined (destroyed, but not
@@ -1270,6 +1273,22 @@ begin
   while pos <= length(s) do result := result+decodeUtf8Char(s, pos);
 end;
 
+
+// NB: Default overload of TArrayHelper<ShortString>.Sort(x) dispatches the specific comparator
+// interface through the following unwieldy chain, to invoke it according to a generic type T:
+//  1: TComparer<T>.Default()
+//  2,3: Generics.Default._LookupVtableInfo*()
+//  4: TComparerService.LookupComparer()
+//  5: TComparerService.SelectShortStringComparer()
+function SSArraySortFunc(constref Left, Right: ShortString): Integer;
+begin
+  // We can't use ShortCompareText() directly as its parameters are 'const', not 'constref', and it
+  // returns SizeInt instead of Integer. The standard TCompare.ShortString() from Generics.Defaults
+  // isn't suitable as well since it's case-sensitive just like the default fpc_shortstr_compare().
+  // https://gitlab.com/freepascal.org/fpc/source/-/issues/41535 - on constref in Generics package
+  // https://gitlab.com/freepascal.org/fpc/source/-/issues/10318 - about adding ShortCompareText()
+  Result := Integer(ShortCompareText(Left, Right));
+end;
 
 // ////////////////////////////////////////////////////////////////////////// //
 // findFileCI eats case-insensitive path, traverses it and rewrites it to a

src/engine/e_log.pas
@@ -205,7 +205,7 @@ begin
       TMsgType.Fatal: write('FATAL: ');
       TMsgType.Warning: write('WARNING: ');
     end;
-    formatstrf(fmt, args, conwriter);
+    formatstrf(fmt, args, @conwriter);
     writeln;
   end;
 
@@ -243,7 +243,7 @@ begin
 
   xlogLastWasEOL := true; // to output prefix
   xlogWantSpace := true; // after prefix
-  formatstrf(fmt, args, logwriter);
+  formatstrf(fmt, args, @logwriter);
   if not xlogLastWasEOL
     then writeln(xlogFile, '')
     else writeln(xlogFile, xlogPrefix);

src/engine/e_sound_fmod.inc
@@ -403,7 +403,7 @@ begin
   if res <> FMOD_OK then
     Exit;
 
-  res := FMOD_Channel_SetCallback(Chan, Channel_Callback);
+  res := FMOD_Channel_SetCallback(Chan, @Channel_Callback);
   if res <> FMOD_OK then
   begin
   end;
@@ -443,7 +443,7 @@ begin
   begin
   end;
 
-  res := FMOD_Channel_SetCallback(Chan, Channel_Callback);
+  res := FMOD_Channel_SetCallback(Chan, @Channel_Callback);
   if res <> FMOD_OK then
   begin
   end;
@@ -483,7 +483,7 @@ begin
   begin
   end;
 
-  res := FMOD_Channel_SetCallback(Chan, Channel_Callback);
+  res := FMOD_Channel_SetCallback(Chan, @Channel_Callback);
   if res <> FMOD_OK then
   begin
   end;
@@ -528,7 +528,7 @@ begin
   begin
   end;
 
-  res := FMOD_Channel_SetCallback(Chan, Channel_Callback);
+  res := FMOD_Channel_SetCallback(Chan, @Channel_Callback);
   if res <> FMOD_OK then
   begin
   end;
@@ -755,7 +755,7 @@ begin
   begin
   end;
 
-  res := FMOD_Channel_SetCallback(FChannel, Channel_Callback);
+  res := FMOD_Channel_SetCallback(FChannel, @Channel_Callback);
   if res <> FMOD_OK then
   begin
   end;

src/engine/e_sound_sdl.inc
@@ -239,7 +239,7 @@ begin
   end;
 
   Mix_AllocateChannels(N_CHANNELS);
-  Mix_ChannelFinished(chanFinished);
+  Mix_ChannelFinished(@chanFinished);
 
   for i := 0 to N_CHANNELS-1 do
   begin

src/flexui/fui_ctls.pas
@@ -1850,7 +1850,7 @@ begin
     end
     else
     begin
-      result := forEachControl(checkFocus);
+      result := forEachControl(@checkFocus);
       if (result = nil) and (wrap) then result := findFirstFocus();
     end;
   end;
@@ -1884,7 +1884,7 @@ begin
     end
     else
     begin
-      forEachControl(checkFocus);
+      forEachControl(@checkFocus);
       if (lastCtl = nil) and (wrap) then lastCtl := findLastFocus();
       result := lastCtl;
       //if (lastCtl <> nil) then writeln('ctl<', lastCtl.className, '>: {', lastCtl.id, '}');
@@ -3618,7 +3618,7 @@ procedure TUIRadioBox.setChecked (v: Boolean);
 
 begin
   mBoolVar^ := v;
-  if v then topLevel.forEachControl(resetGroup);
+  if v then topLevel.forEachControl(@resetGroup);
 end;
 
 
@@ -3657,6 +3657,6 @@ initialization
 
   oldFocus := winFocusCB;
   oldBlur := winBlurCB;
-  winFocusCB := onWinFocus;
-  winBlurCB := onWinBlur;
+  winFocusCB := @onWinFocus;
+  winBlurCB := @onWinBlur;
 end.

src/flexui/fui_gfx_gl.pas
@@ -1452,12 +1452,12 @@ end;
 initialization
   savedGLState := TSavedGLState.Create(False);
   //createFonts();
-  //winFocusCB := onWinFocus;
-  //winBlurCB := onWinBlur;
-  //prerenderFrameCB := onPreRender;
-  postrenderFrameCB := onPostRender;
-  oglInitCB := onInit;
-  oglDeinitCB := onDeinit;
+  //winFocusCB := @onWinFocus;
+  //winBlurCB := @onWinBlur;
+  //prerenderFrameCB := @onPreRender;
+  postrenderFrameCB := @onPostRender;
+  oglInitCB := @onInit;
+  oglDeinitCB := @onDeinit;
 
 finalization
   deleteFonts();

src/flexui/fui_style.pas
@@ -357,9 +357,9 @@ end;
 // ////////////////////////////////////////////////////////////////////////// //
 constructor TStyleSection.Create ();
 begin
-  mVals := THashStrStyleVal.Create(freeValueCB);
-  mHashes := THashStrSection.Create(freeSectionCB);
-  mCtls := THashStrSection.Create(freeSectionCB);
+  mVals := THashStrStyleVal.Create(@freeValueCB);
+  mHashes := THashStrSection.Create(@freeSectionCB);
+  mCtls := THashStrSection.Create(@freeSectionCB);
 end;
 
 

src/game/Doom2DF.lpr
@@ -16,8 +16,6 @@
 {$INCLUDE ../shared/a_modes.inc}
 {$IFNDEF ANDROID} program {$ELSE} library {$ENDIF} Doom2DF;
 
-{$MODESWITCH CLASSICPROCVARS-}  // TODO: Make this default.
-
 // hope this has no negative impact in case of 'library' above
 // https://stackoverflow.com/questions/11716350/effects-of-switching-between-subsystemconsole-to-subsystemwindows-in-a-dll
 // https://docwiki.embarcadero.com/RADStudio/en/Application_type_(Delphi) - against, but anyway

commit 7e8b1195a45e8c0ff06ca2137963a669830954c2

Game: Fix compilation on FPC newer than 3.2.2 that our Nix autobuilder uses
Regression was introduced in dce13ae29ce979b4343cfdd2e1afdb63c34781a4.
Caused by fix of this issue in RTL: https://gitlab.com/freepascal.org/fpc/source/-/issues/41535.

src/shared/utils.pas
@@ -157,10 +157,11 @@ function utf8Valid (const s: AnsiString): Boolean;
 function utf8to1251 (s: AnsiString): AnsiString;
 
 // complies to TComparisonFunc<ShortString> from Generics.Default
-function SSArraySortFunc(constref Left, Right: ShortString): Integer;
+function SSArraySortFunc(
+  {$IF FPC_FULLVERSION > 30202} const {$ELSE} constref {$ENDIF} Left, Right: ShortString): Integer;
 
 // findFileCI takes case-insensitive path, traverses it, and rewrites it to
-// a case-sensetive one (using real on-disk names). return value means 'success'.
+// a case-sensitive one (using real on-disk names). return value means 'success'.
 // if some dir or file wasn't found, pathname is undefined (destroyed, but not
 // necessarily cleared).
 // last name assumed to be a file, not directory (unless `lastIsDir` flag is set).
@@ -1280,7 +1281,8 @@ end;
 //  2,3: Generics.Default._LookupVtableInfo*()
 //  4: TComparerService.LookupComparer()
 //  5: TComparerService.SelectShortStringComparer()
-function SSArraySortFunc(constref Left, Right: ShortString): Integer;
+function SSArraySortFunc(
+  {$IF FPC_FULLVERSION > 30202} const {$ELSE} constref {$ENDIF} Left, Right: ShortString): Integer;
 begin
   // We can't use ShortCompareText() directly as its parameters are 'const', not 'constref', and it
   // returns SizeInt instead of Integer. The standard TCompare.ShortString() from Generics.Defaults